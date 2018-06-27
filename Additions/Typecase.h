3b15511344eb301d9d5879b991e122927a149715
Anders Hansson <anders.pm.hansson@gmail.com>
2018-06-27 11:15:37 +0200

    Added typecase.

---
+++ Additions/Typecase.h
@@ -0,0 +1,104 @@
+// Acceptable and Enclavable Platform Base ✁ until ✂️
+
+/**
+ 
+    Utf-8 Non-regulars: .*｟｠ ⃦ ('Combining double vertical stroke overlay' or
+        'fat-pipe') ✚ ★ ‽ ⸺ \ 𐤟 (PHOENICIAN WORD SEPARATOR) ↩︎ ↹
+    Example: ｟↹↩︎｠★
+    Logic and one-liners: ≅ ≠ ⋍ ~ ⟶ ⟷ ⊂⊄⊆⊈ ⊃ (Małuszyński inclusion) ∩∪ ∈∉
+        ¬∧∨ ≤≥ ≰ ≱ ∞ ∀∃∄ ⊨ ∅ 𝜖 √ ∑∏ ∂𝜕∆ ∇𝛻 ∫∬∭ ¶
+    Greek: 𝛢𝛣𝛤𝛥𝛦𝛧𝛨𝛩𝛪𝛫𝛬𝛭𝛮𝛯𝛰𝛱𝛲𝛳𝛴𝛵𝛶𝛷𝛸𝛹𝛺 𝛼𝛽𝛾𝛿𝜀𝜁𝜂𝜃𝜄𝜅𝜆𝜇𝜈𝜉𝜊𝜋𝜌𝜎𝜏𝜐𝜑𝜒𝜓𝜔 𝜙𝜛
+    Mathematics 𝐼𝑡𝑎𝑙𝑖cs: 𝐴𝐵𝐶𝐷𝐸𝐹𝐺𝐻𝐼𝐾𝐽𝐿𝑀𝑁𝑂𝑃𝑄𝑅𝑆𝑇𝑈𝑉𝑊𝑋𝑌𝑍 𝑎𝑏𝑐𝑑𝑒𝑓𝑔ℎ𝑖𝚤𝑗𝚥𝑘𝑙𝑚𝑛𝑜𝑝𝑞𝑟𝑠𝑡𝑢𝑣𝑤𝑥𝑦𝑧
+    Math. 𝑩𝒐𝒍𝒅 Italics: 𝑨𝑩𝑪𝑫𝑬𝑭𝑮𝑯𝑰𝑱𝑲𝑳𝑴𝑵𝑶𝑷𝑸𝑹𝑺𝑻𝑼𝑽𝑾𝑿𝒀𝒁 𝒂𝒃𝒄𝒅𝒆𝒇𝒈𝒉𝒊𝒋𝒌𝒍𝒎𝒏𝒐𝒑𝒒𝒓𝒔𝒕𝒖𝒗𝒘𝒙𝒚𝒛
+    Math. Sans-serif: 𝟶𝟷𝟸𝟹𝟺𝟻𝟼𝟽𝟾𝟿 𝖠𝖡𝖢𝖣𝖤𝖥𝖦𝖧𝖨𝖩𝖪𝖫𝖬𝖭𝖮𝖯𝖰𝖱𝖲𝖳𝖴𝖵𝖶𝖷𝖸𝖹 𝖺𝖻𝖼𝖽𝖾𝖿𝗀𝗁𝗂𝗃𝗄𝗅𝗆𝗇𝗈𝗉𝗊𝗋𝗌𝗍𝗎𝗏𝗐𝗑𝗒𝗓
+    Math. Sans-serif B𝗼𝗹𝗱: 𝗔𝗕𝗖𝗗𝗘𝗙𝗚𝗛𝗜𝗝𝗞𝗟𝗠𝗡𝗢𝗣𝗤𝗥𝗦𝗧𝗨𝗩𝗪𝗫𝗬𝗭 𝗮𝗯𝗰𝗱𝗲𝗳𝗴𝗵𝗶𝗷𝗸𝗹𝗺𝗻𝗼𝗽𝗾𝗿𝘀𝘁𝘂𝘃𝘄𝘅𝘆𝘇
+    Math. Sans-serif Bold 𝘐𝘵𝘢𝘭𝘪𝘤: 𝘈𝘉𝘊𝘋𝘌𝘍𝘎𝘏𝘐𝘑𝘒𝘓𝘔𝘕𝘖𝘗𝘘𝘙𝘚𝘛𝘜𝘝𝘞𝘟𝘠𝘡 𝘢𝘣𝘤𝘥𝘦𝘧𝘨𝘩𝘪𝘫𝘬𝘭𝘮𝘯𝘰𝘱𝘲𝘳𝘴𝘵𝘶𝘷𝘸𝘹𝘺𝘻
+    Mathematics 𝙼𝚘𝚗𝚘space: 𝙰𝙱𝙳𝙴𝙵𝙶𝙷𝙸𝙹𝙺𝙻𝙼𝙽𝙾𝙿𝚀𝚁𝚂𝚃𝚄𝚅𝚆𝚇𝚈𝚉 𝚊𝚋𝚌𝚍𝚎𝚏𝚐𝚑𝚒𝚓𝚔𝚕𝚖𝚗𝚘𝚙𝚚𝚛𝚜𝚝𝚞𝚟𝚠𝚡𝚢𝚣
+    Models and Mathematics: ⊏ ⊑ ⊢ ⊨, ⊿ ª0 +  ⃨ + ªn₋1 n₍2k₊1₎ ª1⋯ªk ⋱ ⋰ ⋮ ∎ ⇀↽
+    Parenthesis: ⎛⎜⎝   ⎞⎟⎠
+    Matrices and Conditionals: ⎡⎢⎣  ⎤⎥⎦  ⎧⎨⎩  ⎫⎬⎭  ⎪
+    Temporals: ◯ ♢ ▢
+    ^-Graphemes: ␁ ␂ ␚
+    Signals: ⎍ ⎎
+    Chemistry Hobbyist: ⇾ ₀₁₂₃₄₅₆₇₈₉
+    Alt. Math: ± ∓ ∔
+    Big No-no: ≈ ≣ --<'', '' (Slanted dowsing.)
+    Misc¹: ℃ ℉ ® © ™ № º µ Ω ℊ ℑ𝑚 ℜ𝑒 ∁ ≙ ⋈ ⫝ ⦣⧄⧅⧆⧇ ⧈ ⧉ ╳ ☩ ₧ ₥
+    Misc²: ª º ✓ ✔︎ ☑︎☒ ✕ ✖︎ ✗ ✘ ▲◇□▪︎ △□◇▷▽ ○◦●∙⦁
+        à ⬷ The letter 'a'+'COMBINING GRAVE ACCENT'. See ACUTE and CIRCUMFLEX
+        a͟͞z͟͞ ⬷ 'Combining double-macaron' or the 'Bitfields' macraron'.
+        L̵ ⬷ L and 'Combining short stroke overlay'
+        a⃝ ⬷ The letter 'a' + "COMBINING ENCLOSING CIRCLE"
+        ℹ︎ ⬷ 'Information source'
+        i⃝ ⬷ The letter 'i' + "COMBINING ENCLOSING CIRCLE"
+         ⃒⃘ ⬷ 'Combining long vertical line' and 'ring overlay'
+        c⃥ ⬷ 'Combining reverse solidus overlay'
+         ⃫ ⬷ 'COMBINING LONG SOLIDUS OVERLAY'
+ 
+   Footnotes, references and (enum.) lists: ¹₁²₂³₃⁴₄⁵₅⁶₆⁷₇⁸₈⁹₉⁰₀ •○◦・•⁃⦾⦿‣⁌⁍
+ 
+     ╮  ╯ ╭ ╰ ╱ ╲ ╳     ┤ ┌ ├ ┼ ┘ ┐ ─ │ ┴ ┬    ╴╵╷╴
+ 
+      Key   Value   ∃
+     ┾━━━╈━━━━━━━━━┿━┽           ⌏  ⌎   ⌜  ⌝  ⌖  ⏚
+     │k₁ ┃         │✓│           ⌍  ⌌   ⌞  ⌟  ‖
+     └───┸─────────┴─┘
+ 
+          ┌def┐
+          │   │                     ╭─ Leaf 1 ──╮
+     ─abc─┘─┐ ┘           ── Stem ──┤           ├── Again ──
+            └                       ╰─ Leaf 2 ──╯
+ 
+    „ ⃨as illustrated below or above:‟
+ 
+      ∞              ∞
+      ⌠ 🔍😐         \̅
+      ⎮ 𝘦^-𝘹 𝘥𝘹 ≅ √𝘹 + › ⁣⅟k² ≅ @w Sum[, ... ∈ ℝ (DOUBLE-STRUCK CAPITAL R)    (0)
+      ⌡              /͟
+     -∞             k=1
+ 
+      ⎡n₁₁     ⎤    ⎧ ⎫    ⎛   ⎞      ⸑
+      ⎢        ⎥    ⎨ ⎬    ⎜   ⎟     ⎸    ⎲
+      ⎣     nᵣᵥ⎦    ⎩ ⎭    ⎝   ⎠    ⎷    ⎳
+ 
+   Further, consider:
+ 
+   Dashes: ‒ Figure Dash, – En Dash, — Em Dash, ⁓ Swung Dash, ⸺ Two-em Dash,
+        ⸻ Three-em Dash, 〰 Wavy Dash, - Hyphen Minus.
+ 
+   Arrows: ⇽ ⇾ ⟲⟳ ↗︎↘︎↙︎↖︎ ⸔⸕ ⤡⤢ ⤺⤻ ↰↱↲↳ ↴ ↩︎ ↵  ⮐ ↹ ←↑→↓
+     ↼ (Leftward harpoon with barb upwards) ↽↾↿⇀⇁⇂⇃⇄⇅⇆⇇⇈⇉⇊⇋⇌⇍⇏⇐⇑⇒⇓⇚⇛⇜⇝⇤⇥⇦⇧
+     ⇨⇩⇪⇵⇶ ⎔ ➔➙➛➜➝➞➟➠⟰⟤⟥ ⤶ ⤷  ↔︎
+     📲📩📈📉🔃➡︎⬅︎⬆︎⬇︎↩︎⤴︎⤵︎⤴⤵ (牮筏)
+     ⬷ ⤐ (Leftwards two-headed triple dash arrow)
+ 
+   Extensions: ⁌⁍ ⊸ ⫰ ⫱ ⿱⿳⿶  ⃦ a⃮ b⃯ a͍ (Combining left and right arrow below),
+     a̫b̫c̫ ⬷ 'Combining inverted double arch below' a⃨b⃨c⃨ a⃩ a⃪
+     1゚ (Combining Katakana Sound Mark)
+     a ⃫b (Combining long double solidus overlay)
+     aᷣ (Combining latin small letter R rotund)
+     a⳯b⳰c⳱ (The Coptics)
+     ˥ ˦ ˧ ˨ ˩ (Tone letters)
+ 
+    Symbol  c-char  Utf-8          Unicode Name
+       ␜  "\u241C" 0xE2 0x90 0x9C SYMBOL FOR FILE SEPARATOR
+       ␝  "\u241D" 0xE2 0x90 0x9D SYMBOL FOR GROUP SEPARATOR
+       ␞  "\u241E" 0xE2 0x90 0x9E SYMBOL FOR RECORD SEPARATOR
+       ␟  "\u241F" 0xE2 0x90 0x9F SYMBOL FOR UNIT SEPARATOR
+ 
+    ␉ ␋ HORZONTAL and VERTICAL TABULATION ⇀↽ ⇀↽ ⇀↽ ⇀↽ "\u001F" - "\u001C",
+    0x1F - 0x1C, INFORMATION SEPARATOR ONE/FOUR
+ 
+   Buttons: ⌘ Command, &#x2318; – &#8984; ⌥ Option, &#x2325; – &#8997; ⇧ Shift,
+     &#x21E7; – &#8679; ⎋ ESC &#x238B; – &#9099; ⇪ Capslock &#x21ea; – &#8682;
+     ⏎  Return &#x23ce; – &#9166; ⌫ Delete/Backspace, &#x232b; – &#9003;
+     ⇥ Tab Arrow
+ 
+   Spaces: ⇀ ↽ (en space), ⇀  ↽ (em space), ⇀ ↽ (Three-per-em space),
+     ⇀ ↽ (Four per em-space), ⇀ ↽ (Six-per-em), ⇀ ↽ (Figure space),
+     ⇀ ↽ (Punctuation space), ⇀ ↽ (Thin space), ⇀ ↽ (Hair space),
+     ⇀ ​↽ (Zero width space), ⇀ ↽ (Narrow no-break space),
+     ⇀ ↽ (Medium mathematical space), ⇀　↽ (Ideographic space), ⇀〿↽
+     (Ideographic half-fill space). ␣ (Knuths' Constant.)
+ 
+ *␣///}✂️ >> --<shoebox>{Utf-8 Symbols} */
