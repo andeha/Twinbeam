8eabdf192d2b4f8cb98c7632eb82eb394cf6e85e
Anders Hansson <anders.pm.hansson@gmail.com>
2018-06-28 14:55:23 +0200

    Added source file.

---
+++ Source/Utf8.cpp
@@ -0,0 +1,123 @@
+//
+//  Utf8.cpp
+//  Twinbeam
+//
+
+#include <Twinbeam.h>
+
+FOCAL
+__builtin_int_t
+Utf8Followers(uint8_t leadOr8Bit)
+{
+    if (leadOr8Bit < 128) { return 0; }
+    if (128 <= leadOr8Bit && leadOr8Bit < 192) return -1;
+    if (248 <= leadOr8Bit) return -1;
+    
+#ifdef __mips__ // clz $a0, $v0,   ⃨  or clo.
+    __builtin_int_t onesUntilZero = __builtin_clz(~((uint32_t)leadOr8Bit<<24));
+#elif defined __x86_64__ // BSF, BSR, LZCNT, TZCNT, __lzcnt64 on Win64
+    __builtin_int_t onesUntilZero = __builtin_clzll(~((uint64_t)leadOr8Bit<<56));
+#else
+    auto clz = ^(uint8_t x) {
+        INNER_DATA static constexpr uint8_t lookup[16] = { 4, 3, 2, 2, 1, 1, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 0 };
+        uint8_t upper = x >> 4, lower = x & 0x0F;
+        return upper ? lookup[upper] : 4 + lookup[lower];
+    };
+    __builtin_int_t onesUntilZero = clz(~leadOr8Bit);
+#endif
+    
+    return onesUntilZero - 1;
+}
+
+FOCAL
+char32_t
+Utf8ToUnicode(
+    uint8_t *p,
+    __builtin_int_t bytes
+)
+{
+    switch (bytes) { case 1: return (char32_t)(uint8_t)*p; case 2: return
+    (0b11111&*p) << 6 | (0b111111&(*(p + 1))); case 3: return (0b1111&*p) <<
+    12 | (0b111111&(*(p + 1))) << 6 | (0b111111&(*(p + 2))); case 4: return
+    (0b111&*p) << 18 | (0b111111&(*(p + 1))) << 12 | (0b111111&(*(p + 2))) <<
+    6 | (0b111111&(*(p + 3))); } return 0;
+}
+
+FOCAL
+int
+TokenizeUtf8OrUnicode(
+    Encoding encoding,
+    Memoryview content,
+    __builtin_int_t& beam,
+    void (^character)(char32_t unicode, __builtin_int_t byteOffset, bool& stop)
+)
+{
+    __builtin_int_t bytes = content.byteCount;
+    __builtin_int_t i = beam;
+    while (i < bytes) {
+        SemanticPointer<void *> p = content.region->pointer(i + content.bytesOffset);
+        __builtin_int_t charBytes = 4;
+        bool stop = false;
+        if (encoding == Encoding::utf8) {
+            uint8_t *c = (uint8_t *)p.pointer;
+            charBytes = Utf8Followers(*(uint8_t *)c) + 1;
+            if (charBytes == -1) return 1; // Not Utf-8?
+            char32_t unicode = Utf8ToUnicode(c, charBytes);
+            character(unicode, i, stop);
+        } else { character(*(char32_t *)(p.pointer), i, stop); }
+        i += charBytes;
+        beam = i;
+        if (stop) break;
+    }
+    return 0;
+}
+
+FOCAL
+int
+Utf8Sync(
+    uint8_t **p
+) // Backs at most 3 bytes to regain sync.
+{
+    __builtin_int_t i = 3;
+#define Utf8IsLead(x) (((x)&0b11000000)==0xD0) // Ensure argument is uint8_t or equivalent.
+#define Utf8Is8Bit(x) ((x) <= 0x7f)
+#define Utf8IsPointerToFollower(p) (((*p)&0b11000000)==0xC0)
+    while (i && Utf8IsPointerToFollower(*p)) { (*p)--; i--; }
+    return i == 0 || !(Utf8IsLead(**p) || Utf8Is8Bit(**p));
+}
+
+FOCAL
+int
+UnicodeToUtf8(
+    char32_t u,
+    void (^completion)(const uint8_t *p, int bytes)
+)
+{
+    INNER_DATA const unsigned char firstByteMark[7] = { 0x00, 0x00, 0xC0, 0xE0,
+        0xF0, 0xF8, 0xFC };
+    
+    INNER_DATA const char32_t byteMask = 0xBF;
+    INNER_DATA const char32_t byteMark = 0x80;
+    
+    unsigned short bytesToWrite = 0;
+    
+    if (u < (char32_t)0x80) { bytesToWrite = 1; }
+    else if (u < (char32_t)0x800) { bytesToWrite = 2; }
+    else if (u < (char32_t)0x10000) { bytesToWrite = 3; }
+    else if (u <= (char32_t)0x0010FFFF) { bytesToWrite = 4; }
+    else { return 1; }
+    
+    uint8_t target[4];
+    
+    switch (bytesToWrite) {
+        case 4: target[3] = (uint8_t)((u | byteMark) & byteMask); u >>= 6;
+        case 3: target[2] = (uint8_t)((u | byteMark) & byteMask); u >>= 6;
+        case 2: target[1] = (uint8_t)((u | byteMark) & byteMask); u >>= 6;
+        case 1: target[0] = (uint8_t) (u | firstByteMark[bytesToWrite]);
+    }
+    
+    completion(target, bytesToWrite);
+    
+    return 0;
+}
