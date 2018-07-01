4ae25c8a1e6a004b5903954a634623d157a1d9fb
Anders Hansson <anders.pm.hansson@gmail.com>
2018-07-01 09:58:56 +0200

    Improved exposition.

---
+++ pic32rt/pic32mx.hpp
@@ -0,0 +1,33 @@
+//
+//  pic32mx.hpp
+//  pic32rt
+//
+
+#ifndef __PIC32MX_HPP
+#define __PIC32MX_HPP
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-variable"
+PIC32SYMBOL(MX, U5MODE,  0xBF806a00) // UARTx mode register
+PIC32SYMBOL(MX, U5STA,   0xBF806a10) // UARTx status and control register
+PIC32SYMBOL(MX, U5TXREG, 0xBF806a20) // Transmit buffer register in which data to be transmitted is stored
+PIC32SYMBOL(MX, U5RXREG, 0xBF806a30) // Recieve buffer register in which data recieved is stored
+#pragma clang diagnostic pop –
+
+BITMASK (uint32_t) { // UxMODE
+  PIC32MX_U5MODE_MODE_ON = 0b1<<15, // UARTx Enable bit
+};
+
+BITMASK (uint32_t) { // UxSTA
+  PIC32MX_U5STA_UTXEN = 0b1 << 10, // Transmit enable
+  PIC32MX_U5STA_UTXBF = 0b1 <<  9, // Transmit buffer full status
+  PIC32MX_U5STA_OERR  = 0b1 <<  1, // Receive buffer overrun error status
+  PIC32MX_U5STA_URXDA = 0b1 <<  0, // Receive buffer data available (read-only)
+};
+
+#define 🔎𝑀𝑋(symbol) 🔎(PIC32MX_##symbol)
+#define 🔧𝑀𝑋(symbol) 🔧(PIC32MX_##symbol)
+#define 🔧0𝑀𝑋(symbol,msk) 🔧(PIC32MX_##symbol##CLR) = PIC32MX_##symbol##_##msk
+#define 🔧1𝑀𝑋(symbol,msk) 🔧(PIC32MX_##symbol##SET) = PIC32MX_##symbol##_##msk
+
+#endif
