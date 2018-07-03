7c2abdf68deb66c219d99d90552cb1a1504d9781
Anders Hansson <anders.pm.hansson@gmail.com>
2018-07-03 15:40:56 +0200

    Added file.

---
+++ Additions/FontRasterizer.h
@@ -0,0 +1,38 @@
+//
+//  FontRasterizer.h
+//  Twinbeam Additions
+//
+
+#ifndef __FONTRASTERIZER_H
+#define __FONTRASTERIZER_H
+
+#include <Twinbeam.h>
+
+struct FontRasterizer {
+    
+    FontRasterizer(int horizRes, int vertRes);
+    
+    ~FontRasterizer();
+    
+    typedef struct BoundingBox { signed long xMin, yMin, xMax, yMax; } BBox;
+    
+    int renderMonocrome(int width, int height,
+        const char * otfFontUtf8filepath,
+        char32_t (^text)(int index),
+        void (^bitmap)(int width, int rows, uint8_t *buffer, int pitch, int
+          horzKern));
+    
+    int renderAA256(int width, int height,
+        const char * otfFontUtf8filepath,
+        char32_t (^text)(int index),
+        void (^bitmap)(int width, int rows, uint8_t *buffer, int pitch, int
+          horzKern));
+    
+    int renderOutline(int width, int height,
+        const char * otfFontUtf8filepath,
+        char32_t (^text)(int index),
+        void (^outline)(BoundingBox boundingBox, int horzKern));
+    
+😐;
+
+#endif
