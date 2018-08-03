b68da6bce5f58af94904934286dd45f3d175d2b3
Anders Hansson <anders.pm.hansson@gmail.com>
2018-08-03 08:50:09 +0200

    Updated exposition.

--- Source/printf.cpp
+++ Source/printf.cpp
@@ -1,20 +1,31 @@
 //
 //  printf.cpp 
-//  Twinbeam Additions
+//  The MIPS/PIC32 Monitor
 //
 
-#include <Twinbeam.h>
+#include "Monitor.h"
+#include "pic32mx.hpp"
+#include "pic32mz.hpp"
+#include "stable.hpp"
 
-// #define SYMBOL_IS_CHAR_NOT_VARIABLE_BYTE_COUNT_UTF8
+#define SYMBOL_IS_CHAR_NOT_VARIABLE_BYTE_COUNT_UTF8
+
+INNER_FUNCTION
+void
+streamout_char(
+    Outputstream *stream,
+    char c
+)
+{ stream->write((uint8_t *)&c, (__builtin_uint_t)1); }
 
 INNER_FUNCTION
 int
 streamout_eightbit(
-    const char *s,
-    void (^output)(char c)
+    Outputstream *stream,
+    const char *s
 )
 { int printedExcept0 = 0;
-    while (char c = *s++) { output(c); printedExcept0++; }
+    while (char c = *s++) { streamout_char(stream, c); printedExcept0++; }
     return printedExcept0;
 }
 
@@ -23,27 +34,27 @@ streamout_eightbit(
 INNER_FUNCTION
 int
 vfprintf_eightbit(
-    void (^output)(char c),
+    Outputstream *stream,
     const char *format,
     __builtin_va_list arg
 )
 { __block int printedBytesExcept0 = 0;
     while (char c = *format++) {
-        if (c != '%' || (c = *format++) == '%') { output(c); printedBytesExcept0++; }
+        if (c != '%' || (c = *format++) == '%') { streamout_char(stream, c); printedBytesExcept0++; }
         else { switch (c) {
             case 'd': {
                 const __builtin_int_t d = __builtin_va_arg(arg, __builtin_int_t);
-                Base(d, 10, 0, ^(char s) { output(s); printedBytesExcept0++; });
+                Base(d, 10, 0, ^(char s) { streamout_char(stream, s); printedBytesExcept0++; });
                 break; }
             case 'x': {
                 const __builtin_uint_t x = __builtin_va_arg(arg, __builtin_uint_t);
                 Base(x, 16,
 #ifdef __x86_64__
-                     16
+                    16
 #elif defined __mips__
                      8
 #endif
-                , ^(char s) { output(s); printedBytesExcept0++; });
+                , ^(char s) { streamout_char(stream, s); printedBytesExcept0++; });
                 break; }
             case 'b': {
                 const __builtin_uint_t b = __builtin_va_arg(arg, __builtin_uint_t);
@@ -53,27 +64,29 @@ vfprintf_eightbit(
 #elif defined __mips__
                      32
 #endif
-                , ^(char s) { output(s); printedBytesExcept0++; });
+                , ^(char s) { streamout_char(stream, s); printedBytesExcept0++; });
                 break; }
             case 's': {
                 const char *s = __builtin_va_arg(arg, const char *);
-                printedBytesExcept0 += streamout_eightbit(s, output);
+                printedBytesExcept0 += streamout_eightbit(stream, s);
                 break; }
- //         case 'S': {
- //             const char32_t *s = __builtin_va_arg(arg, const char32_t *);
- //             printedBytesExcept0 += streamout_unicodes(stream, s);
- //             break; }
+#ifdef VARIABLE_BYTE_COUNT_UTF8
+            case 'S': {
+                const char32_t *s = __builtin_va_arg(arg, const char32_t *);
+                printedBytesExcept0 += streamout_unicodes(stream, s);
+                break; }
+#endif
         } }
     }
     return printedBytesExcept0;
 }
 
 FOCAL extern "C"
-int printf(const char *eightbitFormat, ...)
+int printf(const char *eightBitFormat, ...)
 {
     int result;
-    va_prologue(eightbitFormat)
-    result = vfprintf_eightbit(^(char c) { putch(c); }, eightbitFormat, arg);
+    va_prologue(eightBitFormat)
+    result = vfprintf_eightbit(&ctx.io, eightBitFormat, arg);
     va_epilogue
     return result;
 }
@@ -83,21 +96,21 @@ int printf(const char *eightbitFormat, ...)
 INNER_FUNCTION
 int
 streamout_unicode(
-    char32_t u,
-    unsigned short (^output)(char *p, short unsigned bytes)
+    Outputstream *stream,
+    char32_t u
 )
-{ __block int bytesstreamed = 0;
+{ __block int bytesStreamed = 0;
     UnicodeToUtf8(u, ^(const uint8_t *p, int bytes) { // On a Microchip PIC32, the fifo is 4 or 8 levels deep.
-        bytesstreamed += output((char *)p, bytes);
+        bytesStreamed += stream->write((uint8_t *)p, bytes);
     });
-    return bytesstreamed;
+    return bytesStreamed;
 }
 
 INNER_FUNCTION
 int // bytes, symbols
 streamout_unicodes(
-    Memoryview *view,
-    unsigned short (^output)(char *p, short unsigned bytes)
+    Outputstream *stream,
+    Memoryview *view
 )
 {
     // __block int symbolsExcept0 = 0;
@@ -108,7 +121,7 @@ streamout_unicodes(
     if (TokenizeUtf8OrUnicode(Encoding::unicode, *view, beam, // Ain't Stoppn' Until/At Dawn
       ^(char32_t unicode, __builtin_int_t byteOffset, bool& stop) {
           if (beam == tetras) stop = true;
-          streamout_unicode(unicode, output); /* symbolsExcept0++; tetras--; */
+          streamout_unicode(stream, unicode); /* symbolsExcept0++; tetras--; */
       })) { }
     
     // view.region->forall(^(SemanticPointer<uint8_t *> isolative, bool first, bool
@@ -123,18 +136,18 @@ streamout_unicodes(
 
 int // Tuple<int, int, int> i.e user-percieved characters, unicodes and utf-8
 vfprintf_unicode(
-    unsigned short (^output)(char *p, short unsigned bytes),
+    Outputstream *stream,
     const char32_t *oneblockFormat,
     __builtin_va_list arg
 )
 {
     __block int printedSymbolsExcept0 = 0;
     while (char32_t c = *oneblockFormat++) {
-        if (c != '%' || (c = *oneblockFormat++) == '%') { streamout_unicode(c, output); printedSymbolsExcept0++; }
+        if (c != '%' || (c = *oneblockFormat++) == '%') { streamout_unicode(stream, c); printedSymbolsExcept0++; }
         else { switch (c) {
             case 'd': {
                 const __builtin_int_t d = __builtin_va_arg(arg, __builtin_int_t);
-                Base(d, 10, 0, ^(char s) { output(&s, 1); printedSymbolsExcept0++; });
+                Base(d, 10, 0, ^(char s) { streamout_char(stream, s); printedSymbolsExcept0++; });
                 break; }
             case 'x': {
                 const __builtin_uint_t x = __builtin_va_arg(arg, __builtin_uint_t);
@@ -144,7 +157,7 @@ vfprintf_unicode(
 #elif defined __mips__
                 8
 #endif
-                , ^(char s) { output(&s, 1); printedSymbolsExcept0++; });
+                , ^(char s) { streamout_char(stream, s); printedSymbolsExcept0++; });
                 break; }
             case 'b': {
                 const __builtin_uint_t b = __builtin_va_arg(arg, __builtin_uint_t);
@@ -154,15 +167,15 @@ vfprintf_unicode(
 #elif defined __mips__
                 32
 #endif
-                , ^(char s) { output(&s, 1); printedSymbolsExcept0++; });
+                , ^(char s) { streamout_char(stream, s); printedSymbolsExcept0++; });
                 break; }
             case 's': {
                 const char *s = __builtin_va_arg(arg, const char *);
-                printedSymbolsExcept0 += streamout_eightbit(s, ^(char c) { output(&c, 1); });
+                printedSymbolsExcept0 += streamout_eightbit(stream, s);
                 break; }
             case 'S': {
                 Memoryview *view = __builtin_va_arg(arg, Memoryview *);
-                printedSymbolsExcept0 += streamout_unicodes(view, output);
+                printedSymbolsExcept0 += streamout_unicodes(stream, view);
                 break; }
         } }
     }
@@ -171,13 +184,13 @@ vfprintf_unicode(
 
 int
 vfprintf_utf8(
-    unsigned short (^output)(char *p, short unsigned bytes),
+    Outputstream *stream,
     const char *utf8Format,
     __builtin_va_list arg
 )
 {
     String s = StringLiteral(utf8Format); // Requires that INLINED static String Literal(const char *utf8, ...) creates a one-block Unicode string, which the method does.
-    int result = vfprintf_unicode(output, (const char32_t *)(*s).region->pointer(0).pointer, arg); // ☜😐 TODO: Consider adding a user-defined Unicode for next block address.
+    int result = vfprintf_unicode(stream, (const char32_t *)(*s).region->pointer(0).pointer, arg); // ☜😐 TODO: Consider adding a user-defined Unicode for next block address.
     return result;
 }
 
@@ -186,7 +199,7 @@ int printf(const char *utf8Format, ...)
 {
     int result;
     va_prologue(utf8Format)
-    result = vfprintf_utf8(^(char * utf8, short unsigned bytes) { unsigned short i = bytes; while (i--) { putch(*(utf8 + i)); } return bytes; }, utf8Format, arg);
+    result = vfprintf_utf8(_myTerminal.outputstream(), utf8Format, arg);
     va_epilogue
     return result;
 }
