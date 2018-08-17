71ca5492d2feba6779735140677584ef230f57ec
Anders Hansson <anders.pm.hansson@gmail.com>
2018-08-17 12:39:32 +0200

    Corrected lapsus.

--- Additions/FontRasterizer.h
+++ Additions/FontRasterizer.h
@@ -17,19 +17,19 @@ struct FontRasterizer {
     typedef struct BoundingBox { signed long xMin, yMin, xMax, yMax; } BBox;
     
     int renderMonocrome(int width, int height,
-        const char * otfFontUtf8filepath,
+        Memoryview otfFont,
         char32_t (^text)(int index),
         void (^bitmap)(int width, int rows, uint8_t *buffer, int pitch, int
           horzKern));
     
     int renderAA256(int width, int height,
-        const char * otfFontUtf8filepath,
+        Memoryview otfFont,
         char32_t (^text)(int index),
         void (^bitmap)(int width, int rows, uint8_t *buffer, int pitch, int
           horzKern));
     
     int renderOutline(int width, int height,
-        const char * otfFontUtf8filepath,
+        Memoryview otfFont,
         char32_t (^text)(int index),
         void (^outline)(BoundingBox boundingBox, int horzKern));
     
