ab9464fbc34cb6710336573b193c2c483b5cca3c
Anders Hansson <anders.pm.hansson@gmail.com>
2018-08-20 11:42:58 +0200

    Corrected lapsus.

--- Additions/ModernTask.h
+++ Additions/ModernTask.h
@@ -6,16 +6,16 @@
 #ifndef __MODERNTASK_H
 #define __MODERNTASK_H
 
-#include <Pinecone.h>
+#include <Twinbeam.h>
 
 struct Multitasks {
     
     typedef uint8_t CpuState[512] __attribute__((aligned(16)));
     
     typedef struct Task {
-        uint64_t rip;
-        uint64_t rsp;
-        uint64_t stack_size;
+        __builtin_uint_t rip;
+        __builtin_uint_t rsp;
+        __builtin_uint_t stack_size;
         // libthread_ucontext fiber;
         CpuState state;
         jmp_buf2 exception;
