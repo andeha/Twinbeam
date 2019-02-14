0e24d57f9ee24939478ef19022293bfb6e1d8d93
Anders Hansson <anders.pm.hansson@gmail.com>
2019-02-14 12:21:11 +0100

    Corrected lapsus.

--- Additions/😐🍄-Silmarils/Kirkbridge/911.h
+++ Additions/😐🍄-Silmarils/Kirkbridge/911.h
@@ -8,17 +8,20 @@ struct µProcˢ { Octa aux, acc; bool overflow; };
 enum { 𝚣𝚎𝚛𝚘, 𝚛𝟷, 𝚛𝟸, 𝚛𝟹, 𝚛𝟺, 𝚛𝟻, 𝚛𝟼, 𝚛𝚊𝚡, 𝚌𝚜𝟶, 𝚌𝚜𝟷, 𝚌𝚜𝟸, 𝚌𝚜𝟹,
   𝚊𝚌𝚌𝟹𝚑𝚒, 𝚊𝚌𝚌𝟹𝚕𝚘, 𝚊𝚌𝚌𝟸𝚑𝚒, 𝚊𝚌𝚌𝟸𝚕𝚘, 𝚊𝚌𝚌𝟷𝚑𝚒, 𝚊𝚌𝚌𝟷𝚕𝚘, 𝚊𝚌𝚌𝟶𝚑𝚒, 𝚊𝚌𝚌𝟶𝚕𝚘 };
 
-BITMASK (uint8_t) { 𝙰𝙲𝙲_𝚘𝚟𝚎𝚛𝚏𝚕𝚘𝚠 = 0b1<<3, 𝙰𝙲𝙲_𝚌𝚊𝚛𝚛𝚢ₒ = 0b1<<2, 𝙰𝙲𝙲_𝚌𝚊𝚛𝚛𝚢ᵢ =
-  0b1<<1, 𝙰𝙲𝙲_𝚣𝚎𝚛𝚘 = 0b1<<0 };
+BITMASK (uint8_t) { 𝚛𝚎𝚐𝚜_𝚘𝚟𝚎𝚛𝚏𝚕𝚘𝚠 = 0b1<<3, 𝚛𝚎𝚐𝚜_𝚌𝚊𝚛𝚛𝚢ₒ = 0b1<<2, 𝚛𝚎𝚐𝚜_𝚌𝚊𝚛𝚛𝚢ᵢ =
+  0b1<<1, 𝚛𝚎𝚐𝚜_𝚣𝚎𝚛𝚘 = 0b1<<0 };
 
 union Register { __uint128_t u; __int128_t s; uint64_t octas[2]; uint32_t
   tetras[4]; uint16_t halfs[8]; uint8_t bytes[16]; };
 
-struct Registerset { 𝚏𝚙, 𝚜𝚙; Register gprs[𝚊𝚌𝚌𝟶𝚕𝚘+1], 𝚏𝚕𝚊𝚐𝚜; };
+struct Registerset { uint64_t 𝚏𝚙, 𝚜𝚙; Register gprs[𝚊𝚌𝚌𝟶𝚕𝚘+1], 𝚏𝚕𝚊𝚐𝚜; };
 
-struct µProcᵖ { uint64_t 𝚙𝚌; void (*irq)(); uint8_t set; Registerset regs[7]; };
+BITMASK (uint8_t) { 𝚍𝚎𝚋𝚞𝚐_𝚘𝚗 = 0b1<<0 };
 
-typedef int gpr;
+struct µProcᵖ { uint64_t 𝚙𝚌; void (*irq)(); uint32_t 𝚍𝚎𝚋𝚞𝚐; uint8_t set;
+  Registerset regs[7]; };
+
+typedef short gpr;
 
 namespace Op {
 
@@ -57,6 +60,7 @@ extern void (^𝚛𝚍𝚙𝚐𝚙𝚛)(gpr r_src_prev, gpr r_dst_curr);
 extern void (^𝚜𝚝𝚊𝚛𝚝)();
 extern void (^𝚌𝚘𝚖𝚖𝚒𝚝)();
 extern void (^𝚊𝚋𝚘𝚛𝚝)();
+extern void (^𝚛𝚎𝚐𝚜𝚗𝚊𝚙)(gpr r);
 extern void (^𝚜𝚑𝚊𝚍𝚘𝚠)(uint8_t number);
 
 }
