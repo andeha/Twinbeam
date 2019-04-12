f9346c9834eaccc88ff6f8d4201afab1fe04bcc0
Anders Hansson <anders.pm.hansson@gmail.com>
2019-04-12 18:25:36 +0200

    Corrected lapsus.

--- Additions/😐🍄-Silmarils/🥽 Optics.cpp
+++ Additions/😐🍄-Silmarils/🥽 Optics.cpp
@@ -2,15 +2,15 @@
 
 #include <System.h>
 
-Fiber::fiber_t 😎₁, 💡, 😎₂; /* ⟿ */
+Fiber::fiber_t 😎₁ᵪ, 💡ᵪ, 😎₂ᵪ; /* ⟿ */
 
 #define TEXT(name) __attribute__ ((section(".text,." name)))
 #define COMMON __attribute__ ((section(".data,.data")))
 #define LOCAL(name) __attribute__ ((section(".data,." name)))
 
-static void 😎₁ᵪ(void * arg) { for (;;) { … } }            TEXT("🍏")
-static void 😎₂ᵪ(void * arg) { for (;;) { … } }            TEXT("🍎")
-static void 💡ᵪ(void * arg) { for (;;) { … } }             TEXT("🌳")
+static void 😎₁(void * arg) { for (;;) { … } }            TEXT("🍏")
+static void 😎₂(void * arg) { for (;;) { … } }            TEXT("🍎")
+static void 💡(void * arg) { for (;;) { … } }             TEXT("🌳")
 
 int
 main(
@@ -19,9 +19,9 @@ main(
 )
 {
     switch (__builtin_coreid()) {
-    case 0: Load("🍏", 0); Fiber::create(😎₁, 😎₁ᵪ, 0); 🎼(😎₁); break;
-    case 1: Load("🍎", 1); Fiber::create(😎₂, 😎₂ᵪ, 0); 🎼(😎₂); break;
-    case 2: Load("🌳", 2); Fiber::create(💡,  💡ᵪ,  0); 🎼(💡);  break;
+    case 0: Load("🍏", 0); Fiber::create(😎₁ᵪ, 😎₁, 0); 🎼(😎₁ᵪ); break;
+    case 1: Load("🍎", 1); Fiber::create(😎₂ᵪ, 😎₂, 0); 🎼(😎₂ᵪ); break;
+    case 2: Load("🌳", 2); Fiber::create(💡ᵪ,  💡,  0); 🎼(💡ᵪ);  break;
     } /* TODO: Add cores, improve the API and jot down the details. */
     return 0; /* (Also attempt to add HIDDEN `/​*` and `*​/` Unicodes to C++). */
 } /* ...and '\n' that renders at design-time as ␣. */
