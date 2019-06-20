056388afd664b3427c543a5f9f267d6e2ed6b20e
Anders Hansson <anders.pm.hansson@gmail.com>
2019-06-20 17:41:56 +0200

    Updated exposition.

--- Sprinkle/pdb/pdb.h
+++ Sprinkle/pdb/pdb.h
@@ -3,15 +3,12 @@
 //  pdb
 //
 
-struct Bitfield { const char32_t * ident; __builtin_uint_t mask; const char32_t
-  * text; }; typedef Vector<Bitfield> Register; struct AnnotatedRegister {
-  const char32_t * header; const Register& r; __builtin_uint_t init; const
-  char32_t * footer; };
+struct Bitfield { const char32_t * ident; __builtin_uint_t mask; 
+  const char32_t * text; };
 
-extern void (^Putch)(char utf8);
-extern void (^Put)(char32_t unicode);
+typedef Bitfield Register[];
 
-inline bool EightBitIsPrefixOrEqual(const char *eightbitString, const char * eightbitPrefix)
-{ if (!*eightbitString) { return false; } while (*eightbitString && *eightbitPrefix) {
-  if(*eightbitString++ != *eightbitPrefix++) return false; } return true; }
+struct AnnotatedRegister { const char32_t * header; int regcnt;
+  const Bitfield * regs; __builtin_uint_t init; const char32_t * footnote; };
+
 
