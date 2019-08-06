9d9fb4a3ccc7d83f29ade3d1f2df473a5bebd8f4
Anders Hansson <anders.pm.hansson@gmail.com>
2019-08-06 13:21:58 +0200

    Corrected lapsus.

--- Additions/😐🍄-Silmarils/🥽 Restart.cpp
+++ Additions/😐🍄-Silmarils/🥽 Restart.cpp
@@ -14,21 +14,21 @@ main(
 )
 {
     __builtin_uint_t rcon = 🔎𝑀𝑍𝐷𝐴(RCON);
-    if (rcon & HVD1V8R) { print("VDDR1V8 (DDR2) High Voltage Detect\n"); }
-    if (rcon & BCFGERR) { print("Primary Configuration Registers Error\n"); }
-    if (rcon & BCFGFAIL) { print("Primary/Secondary Configuration Registers Error\n"); }
-    if (rcon & VBPOR) { print("VBPOR Mode\n"); }
-    if (rcon & VBAT) { print("VBAT Mode\n"); }
-    if (rcon & DPSLP) { print("Deep Sleep Mode\n"); }
-    if (rcon & CMR) { print("Configuration Mismatch Reset\n"); }
-    if (rcon & EXTR) { print("External Reset (MCLR) Pin\n"); }
-    if (rcon & SWR) { print("Software Reset\n"); }
-    if (rcon & DMTO) { print("Deadman Timer Time-out\n"); }
-    if (rcon & WDTO) { print("Watchdog Timer Time-out\n"); }
-    if (rcon & SLEEP) { print("Wake From Sleep\n"); }
-    if (rcon & IDLE) { print("Wake From Idle\n"); }
-    if (rcon & BOR) { print("Brown-out Reset\n"); }
-    if (rcon & POR) { print("Power-on Reset\n"); }
+    if (🐛𝑀𝑍𝐷𝐴(rcon,HVD1V8R)) { print("VDDR1V8 (DDR2) High Voltage Detect\n"); }
+    if (🐛𝑀𝑍𝐷𝐴(rcon,BCFGFAIL)) { print("Primary Configuration Registers Error\n"); }
+    if (🐛𝑀𝑍𝐷𝐴(rcon,BCFGFAIL)) { print("Primary/Secondary Configuration Registers Error\n"); }
+    if (🐛𝑀𝑍𝐷𝐴(rcon,VBPOR)) { print("VBPOR Mode\n"); }
+    if (🐛𝑀𝑍𝐷𝐴(rcon,VBAT)) { print("VBAT Mode\n"); }
+    if (🐛𝑀𝑍𝐷𝐴(rcon,DPSLP)) { print("Deep Sleep Mode\n"); }
+    if (rcon & PIC32MZDA_RCON_CMR) { print("Configuration Mismatch Reset\n"); }
+    if (🐛𝑀𝑍𝐷𝐴(rcon,EXTR)) { print("External Reset (MCLR) Pin\n"); }
+    if (🐛𝑀𝑍𝐷𝐴(rcon,SWR)) { print("Software Reset\n"); }
+    if (🐛𝑀𝑍𝐷𝐴(rcon,DMTO)) { print("Deadman Timer Time-out\n"); }
+    if (🐛𝑀𝑍𝐷𝐴(rcon,WDTO)) { print("Watchdog Timer Time-out\n"); }
+    if (🐛𝑀𝑍𝐷𝐴(rcon,SLEEP)) { print("Wake From Sleep\n"); }
+    if (🐛𝑀𝑍𝐷𝐴(rcon,IDLE)) { print("Wake From Idle\n"); }
+    if (🐛𝑀𝑍𝐷𝐴(rcon,BOR)) { print("Brown-out Reset\n"); }
+    if (🐛𝑀𝑍𝐷𝐴(rcon,POR)) { print("Power-on Reset\n"); }
     
     while (1) { ; }
     
