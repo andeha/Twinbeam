00c8265aa5fd499249367e04221a0c8146714515
Anders Hansson <anders.pm.hansson@gmail.com>
2019-09-11 05:13:19 +0200

    Added material.

---
+++ Source/pic32mz.cpp
@@ -0,0 +1,57 @@
+/*  𝙋𝙞𝙘𝟯𝟮𝙢𝙯.cpp | Twinbeam. */
+
+#include <Twinbeam.h>
+
+#define ONLY_MSPACES        0
+#define LACKS_SYS_TYPES_H   1
+#define NO_MALLOC_STATS     1
+#define USE_LOCKS           0
+#define LACKS_ERRNO_H       1
+#define LACKS_TIME_H        1
+#define LACKS_STDLIB_H      1
+#define LACKS_STRING_H      1
+#define HAVE_MMAP           0
+#define LACKS_UNISTD_H      1
+#define LACKS_SYS_PARAM_H   1
+#define MALLOC_FAILURE_ACTION
+#define ABORT
+#define EINVAL              22
+#define ENOMEM              12
+typedef long int ptrdiff_t;
+extern "C" void * memset(void * b, int c, size_t len);
+extern "C" void * memcpy(void * dest, void const * src, size_t n);
+void * sbrk(ptrdiff_t incr) { return 0; }
+#include "malloc.cxx"
+
+/* µA("mips", "r2", x₃, x₄) */
+FOCAL
+ByteAlignedRef
+Copy8Memory(
+  ByteAlignedRef dst,
+  ByteAlignedRef src,
+  __builtin_int_t bytes
+) /* A․𝘬․a `memcpy`. */
+{
+    char *dp = (char *)dst;
+    const char *sp = (const char *)src;
+    while (bytes--) *dp++ = *sp++;
+    return dst;
+}
+
+/* µA("mips", "r2", x₃, x₄) */
+FOCAL
+int
+Compare8Memory(
+  ByteAlignedRef l,
+  ByteAlignedRef r,
+  __builtin_uint_t bytes
+) /* A․𝘬․a `memcmp`. See also --<Additions.h>{OptimisticAsync8Copy} i․e copying using DMA. */
+{
+    const unsigned char *p1 = l, *p2 = r;
+    while (bytes--) {
+        if (*p1 != *p2) return *p1 - *p2;
+        else p1++,p2++;
+    }
+    return 0;
+} /* Notice that this method is 'too cristalline' when dealing with Unicodes. */
+
