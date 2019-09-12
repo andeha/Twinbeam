6cf5d251e0d86c288257b12104529c07495418d3
Anders Hansson <anders.pm.hansson@gmail.com>
2019-09-12 21:57:49 +0200

    Improved exposition.

--- Additions/😐🍄-Silmarils/🥽 𝙋𝙧𝙞𝙣𝙩⁺.cpp
+++ Additions/😐🍄-Silmarils/🥽 𝙋𝙧𝙞𝙣𝙩⁺.cpp
@@ -2,40 +2,41 @@
 
 #include <Twinbeam.h>
 
-/* Compile with xcrun clang @ccargs_macosᵧ -o Print⁺ '🥽 ᷣ𝙋𝙧𝙞𝙣𝙩⁺.cpp'   \
-  ../../Source/Releases/libTwinbeam_macOS.a ../../Source/llvmʳᵗ³.cpp */
+/* Compile with xcrun clang @ccargs_macosᵧ -o Print⁺                       \
+    ../../Source/llvmʳᵗ³.cpp '🥽 𝙋𝙧𝙞𝙣𝙩⁺.cpp' */
 
-/* clang++ -g -o Print⁺ -I ../../ -fno-rtti -fblocks -fno-exceptions   \
-    -std=c++2a '🥽 ᷣ𝙋𝙧𝙞𝙣𝙩⁺.cpp' ../../Source/llvmʳᵗ³.cpp ../../Source/Utf8.cpp \
-    ../../Source/System.cpp */
+/* clang++ -g -o Print⁺ -I ../../ -fno-rtti -fblocks -fno-exceptions       \
+    -std=c++2a  ../../Source/Utf8.cpp ../../Source/Memory.cpp              \
+    ../../Source/System.cpp ../../Source/llvmʳᵗ³.cpp '🥽 𝙋𝙧𝙞𝙣𝙩⁺.cpp'  */
 
 DISORDERABLE extern void CastᵈᵇˡToText(double value,
   void (^digits)(bool neg, int e, const char *𝟶to𝟿s),
   void (^zero)(), void (^inf)(), void (^nan)()) { zero(); }
 
-#define ⁺⁼PrintArgAndPop(C,ARG)                                             \
-/* DISORDERABLE */ auto printArgAndPop = ^(char32_t c,                      \
-      __builtin_va_list arg) {                                              \
-    const Argᴾ a = __builtin_va_arg(arg, Argᴾ);                             \
-    switch (a.kind) {                                                       \
-    case 1: out𝕫(a.value.d); break;                                         \
-    case 2: out𝕟(a.value.x); break;                                         \
-    case 3: ᵇⁱⁿᵃʳʸout(a.value.b); break;                                      \
-    case 4: streamout_utf8(a.value.utf8); break;                            \
-    case 5: streamout_unicodes(a.value.ucs.tetras, a.value.ucs.unicodes); break; \
-    case 6: out₂((const char *)&(a.value.c), 1); break;                     \
-    case 7: streamout_unicode(a.value.uc); break;                           \
-    case 9: out𝕕(a.value.f₁); break;                                        \
-    case 8: out𝕕(double(a.value.f₂)); break;                                \
-    case 11: out¹²⁸𝕫(a.value.d); break;                                     \
-    case 12: out¹²⁸𝕟(a.value.d); break;                                     \
-    case 13: out𝕫(a.value.d); break;                                        \
-    default: /* if (a.kind >= 0) imprint[a.kind](a); else */                \
-      streamout_unicode(U'?'); break;                                       \
-    }                                                                       \
-}; printArgAndPop(C,ARG) /* Lexical lambda implicits: `out𝕫`, `out𝕟`, 
-  `ᵇⁱⁿᵃʳʸout`, `streamout_utf8`, `streamout_unicodes`, `out₂`, 
-  `streamout_unicode`, `out𝕕`, `out¹²⁸𝕫` and `out¹²⁸𝕟`. */
+DISORDERABLE extern void reᵍsPrint(__builtin_uint_t mask)
+{
+#ifdef __x86_64__
+#elif defined __mips__
+#endif
+} /* Print at least 0 to 31 non high-volatile registers. */
+
+#define ⁺⁼PrintArgAndPop /* DISORDERABLE */                                 \
+  const Argᴾ a = __builtin_va_arg(arg, Argᴾ);                               \
+  switch (a.kind) {                                                         \
+  case 1: out𝕫(a.value.d); break;                                           \
+  case 2: out𝕟(a.value.x); break;                                           \
+  case 3: ¹⁰¹¹⁰out(a.value.b); break;                                       \
+  case 4: streamout_utf8(a.value.utf8); break;                              \
+  case 5: streamout_unicodes(a.value.ucs.tetras, a.value.ucs.unicodes); break; \
+  case 6: streamout_char(a.value.c); break;                                 \
+  case 7: streamout_unicode(a.value.uc); break;                             \
+  case 8: out𝕕(double(a.value.f₂)); break;                                  \
+  case 9: out𝕕(a.value.f₁); break;                                          \
+  case 11: out¹²⁸𝕟(a.value.U); break;                                       \
+  case 12: out¹²⁸𝕫(a.value.I); break;                                       \
+  case 13: reᵍsPrint(a.value.x); break;                                     \
+  default: /* if (a.kind >= 0) imprint[a.kind](a); else */                  \
+    streamout_unicode(U'?'); break; }
 
 Argᴾ ﹟d(__builtin_int_t d) { return Argᴾ { .value.d=d, 1 }; }
 Argᴾ ﹟x(__builtin_uint_t x) { return Argᴾ { .value.x=x, 2 }; }
@@ -46,7 +47,7 @@ Argᴾ ﹟c(char c) { return Argᴾ { .value.c=c, 6 }; }
 Argᴾ ﹟C(char32_t C) { return Argᴾ { .value.uc=C, 7 }; }
 Argᴾ ﹟U(__uint128_t U) { return Argᴾ { .value.U=U, 11 }; }
 Argᴾ ﹟I(__int128_t I) { return Argᴾ { .value.I=I, 12 }; }
-Argᴾ ﹟reᵍ(__builtin_int_t r) { return Argᴾ { .value.d=r, 13 }; } /* Print register 0-31. */
+Argᴾ ﹟reᵍs(__builtin_uint_t mask) { return Argᴾ { .value.x=mask, 13 }; }
 
 /* …and for the IEEE754's in --<Additions.h> */
 MACRO Argᴾ ﹟F(double f) { return Argᴾ { .value.f₁=f, 9 }; }
@@ -95,14 +96,14 @@ inexorable void Base𝕫(__int128_t ℤ, unsigned short base, unsigned short
   Base𝕟((__builtin_uint_t)ℤ, base, digitsOr0, out); };
 
 inexorable
-int 
+int /* Returns the number of emitted bytes, not the number of emitted frames. */
 print⁺⁺(
   void (^out)(uint8_t * utf8s, short unsigned bytes),
   const char * utf8format,
   __builtin_va_list arg
 )
 {  __builtin_int_t i=0, incr; short followers; char32_t uc;
-    int 𝑓𝑙𝑢𝑐𝑡𝑢𝑎𝑛𝑡 printedSymbolsExcept0=0;
+    int 𝑓𝑙𝑢𝑐𝑡𝑢𝑎𝑛𝑡 printedSymbolsExcept0=0; /* bool may𝘖𝘳DidEscape=false; */
     auto out₂ = ^(const char * utf8s, uint16_t bytes) { out((uint8_t *)utf8s, bytes); printedSymbolsExcept0 += bytes; };
     auto out𝕫 = ^(__builtin_int_t x) { Base𝕫(x, 10, 0, ^(char s) { out₂(&s, 1);  }); };
     auto out𝕟 = ^(__builtin_uint_t x) { Base𝕟(x, 16,
@@ -112,14 +113,15 @@ print⁺⁺(
        8
 #endif
       , ^(char s) { out₂(&s, 1); }); };
-    auto ᵇⁱⁿᵃʳʸout = ^(__builtin_uint_t b) { Base𝕟(b, 2,
+    auto ¹⁰¹¹⁰out = ^(__builtin_uint_t b) { Base𝕟(b, 2,
 #ifdef __x86_64__
        64
 #elif defined __mips__
        32
 #endif
       , ^(char s) { out₂(&s, 1); }); };
-    auto streamout_utf8 = ^(const char * utf8) { char * p=(char *)utf8; while (*p++) { out₂(p, 1); } };
+    auto streamout_char = ^(char c) { out₂(&c, 1); };
+    auto streamout_utf8 = ^(const char * utf8) { char *p=(char *)(utf8); while (*p) { out₂(p++, 1); } }; /* for (char * p=(char *)utf8; *p; p++) { out₂(p, 1); } */
     /* #ifndef AVOID_IEEE754 */
     auto out𝕕 = ^(double ℝ) { const char *zero="0", *inf="∞", *nan="NaN", 
       *minus="-", *decimal="."; 𝑓𝑙𝑢𝑐𝑡𝑢𝑎𝑛𝑡 int count=0;
@@ -138,7 +140,7 @@ print⁺⁺(
       beam=0; while (beam < tetras) { char32_t uc = *(unicodes + beam); streamout_unicode(uc); 
       ++beam; } }; /* { int, (bytes, symbols) } */
     auto out¹²⁸𝕫 = ^(__int128_t I) { Base𝕫(I, 10, 0, ^(char 𝟶to𝟿) { out₂(&𝟶to𝟿, 1); }); };
-    auto out¹²⁸𝕟 = ^(__uint128_t U) { Base𝕟(U, 10, 0, ^(char 𝟶to𝟿and₋) { out₂(&𝟶to𝟿and₋, 1); }); };
+    auto out¹²⁸𝕟 = ^(__uint128_t U) { Base𝕟(U, 16, 0, ^(char 𝟶to𝟿and₋) { out₂(&𝟶to𝟿and₋, 1); }); };
 again:
     auto leadOr8Bit = (uint8_t *)utf8format + i;
     if (*leadOr8Bit == 0x0) { goto unagain; }
@@ -147,8 +149,8 @@ again:
     incr = followers + 1;
     uc = Utf8ToUnicode(leadOr8Bit, incr);
     if (uc == 0xFFFE || uc == 0xFFFF) { return -2; }
-    else if (uc != U'⬚') { streamout_unicode(uc); }
-    else { ⁺⁼PrintArgAndPop(uc, arg); }
+    else if (uc != U'⬚') { streamout_unicode(uc); /* may𝘖𝘳DidEscape = (uc == U'/'); */ }
+    else { ⁺⁼PrintArgAndPop() }
     i += incr; goto again;
 unagain:
     return printedSymbolsExcept0;
@@ -159,7 +161,7 @@ int
 print⁺⁺(const char * utf8format, ...) /* Here all variable args are of the type `Arg`. */
 {  int y;
     va_prologue(utf8format);
-    extern void (*Putₒ)(uint8_t * utf8s, uint16_t bytes);
+    extern void (^Putₒ)(uint8_t * utf8s, uint16_t bytes);
     auto out = ^(uint8_t * utf8s, uint16_t bytes) { Putₒ(utf8s, bytes); };
     y = print⁺⁺(out, utf8format, __arg);
     va_epilogue
@@ -190,6 +192,7 @@ print⁺⁺(
 #pragma mark - Main entry point
 
 extern "C" int write(int fd, const void *s, short unsigned b);
+auto Putₒ2 = ^(uint8_t * utf8s, uint16_t bytes) { write(1, (const void *)utf8s, bytes); };
 
 int
 main(
@@ -199,20 +202,21 @@ main(
 {
     /* Here with the introduction of Unicode `%` becomes redundant, formatting is on the 
         outside and types are checked instead of 'reported' at runtime. */
+    print⁺⁺("Welcome to print: ⬚ ⬚ ⬚\n", ﹟d(12), ﹟x(12), ﹟F(12.1));
     
-    auto Putₒ2 = ^(uint8_t * utf8s, uint16_t bytes) { write(1, (const void *)utf8s, bytes); };
-    print⁺⁺(Putₒ2, "Welcome to print: ⬚ ⬚ ⬚\n", ﹟d(12), ﹟x(12), ﹟F(12.1));
-    
-    __uint128_t x₁ = 100; __int128_t x₂ = -100;
-    print⁺⁺(Putₒ2, "Size of x₁ is ⬚ value is 0x⬚\n", ﹟d(sizeof(x₁)), ﹟U(x₁));
-    print⁺⁺(Putₒ2, "Size of x₂ is ⬚ value is ⬚\n", ﹟d(sizeof(x₂)), ﹟I(x₂));
+    __uint128_t x₁ =  0x4321432143214321; __int128_t x₂ = -100;
+    print⁺⁺("Size of x₁ is ⬚ value is 0x⬚\n", ﹟d(sizeof(x₁)), ﹟U(x₁));
+    print⁺⁺("Size of x₂ is ⬚ value is ⬚\n", ﹟d(sizeof(x₂)), ﹟I(x₂));
+    print⁺⁺("An ⬚ escaped\n", ﹟s("argument ⬚"));
+    print⁺⁺("A unicode ⬚ is escaped\n", ﹟S(1, U"⬚"));
+    print⁺⁺(Putₒ2, "A character ⬚ has landed\n", ﹟c('x'));
     
     return 0;
 }
 
 #pragma mark - Custom types
 
-/* Copy in --<Fossilate.h> 
+/* Copy in --<Fossilate.h>
 Argᴾ ﹟🔗ᵘᵗf⁸(int tetras, const char32_t * pathFileCanonicalᵚ) { 
   return AArgᴾ { .value.ucs={pathFileCanonicalᵚ,tetras}, 13 }; }
 #include <Additions/😐🍄-Silmarils/💸.hpp>
