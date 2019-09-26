0046d7ed3b77359951ec5f17d13c0988e61cfa71
Anders Hansson <anders.pm.hansson@gmail.com>
2019-09-26 12:31:51 +0200

    Improved exposition.

--- Source/llvmʳᵗ³.cpp
+++ Source/llvmʳᵗ³.cpp
@@ -5,7 +5,7 @@
 void * __gxx_personality_v0;
 void * _NSConcreteStackBlock[32];
 void * _NSConcreteGlobalBlock[32];
-extern "C" void __cxa_pure_virtual() { BLURT("Pure virtual function called"); while (1); }
+extern "C" void __cxa_pure_virtual() { print("Pure virtual function called\n"); while (1); }
 extern "C" int __cxa_atexit(void (* fn)(void *), void * arg, void * dso_handle) { return 0; }
 struct { __builtin_int_t board₁, palm₂; } cxaGuard;
 extern "C" int __cxa_guard_acquire() { return 🔒(cxaGuard); }
@@ -58,6 +58,32 @@ DISORDERABLE auto WaitTerminal = ^(int periods𝘖𝘳Zero, int
 
 enum { BLOCK_IS_FLUCTUANT=8, BLOCK_IS_WEAK=16, BLOCK_IS_ANOTHERBLOCK=7 };
 
-void _Block_object_assign(void * dst, const void * obj, const int flags) { }
+extern "C" void _Block_object_assign(void * dst, const void * obj, const int flags)
+{}
 
-void _Block_object_dispose(const void * obj, const int flags) { }
+extern "C" void _Block_object_dispose(const void * obj, const int flags)
+{
+#ifdef XXX
+    if (flags & BLOCK_FIELD_IS_BYREF)  {
+       /* Get rid of the fluctuant data structure held in a Block. */
+       _Block_byref_release(object);
+    }
+    else if ((flags & (BLOCK_FIELD_IS_BLOCK|BLOCK_BYREF_CALLER)) == BLOCK_FIELD_IS_BLOCK) {
+       /* Get rid of a referenced Block held by this Block. (Ignore fluctuant 
+         Block variables, compiler doesn't need to call us.) */
+       _Block_destroy(object);
+    }
+    else if ((flags & (BLOCK_FIELD_IS_WEAK|BLOCK_FIELD_IS_BLOCK|BLOCK_BYREF_CALLER)) == BLOCK_FIELD_IS_OBJECT) {
+       /* Get rid of a referenced object held by this Block. (Ignore fluctuant object 
+        variables, compiler doesn't need to call us.) */
+       _Block_release_object(object);
+    }
+#endif
+}
+extern int fractions(__uint128_t num, __uint128_t denom, __uint128_t &ℕ, __uint128_t &modula);
+extern "C" __uint128_t __udivmodti4(__uint128_t a, __uint128_t b, __uint128_t * rem) {
+  __uint128_t ℕ; int y = fractions(a, b, ℕ, (__uint128_t&)rem); return ℕ; } /* Returns 'a/b' and 'a mod b'. */
+extern "C" __uint128_t __umodti3(__uint128_t a, __uint128_t b) { __uint128_t r; __udivmodti4(a, b, &r); return r; }
+extern "C" __uint128_t __udivti3(__uint128_t a, __uint128_t b) { __uint128_t r, d; d = __udivmodti4(a, b, &r); return d; }
+
+
