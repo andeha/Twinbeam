202f61d55a9de0855dfb6fbfd5ab4f4f8511aa13
Anders Hansson <anders.pm.hansson@gmail.com>
2019-09-27 07:13:13 +0200

    Added material.

---
+++ Additions/😐🍄-Silmarils/🥽 Badge.cpp
@@ -0,0 +1,24 @@
+/*  Badge.cpp | Silver is gold. */
+
+#include <Twinbeam.h>
+#include <Additions/Additions.h>
+
+/* xcrun clang @ccargs_macᵧ² -o Badge '🥽 Badge.cpp'          \
+  ../../Source/llvmʳᵗ³.cpp ../../Additions/Ieee754₆₄bits.cpp */
+
+int
+main(
+  int argc,
+  const char * argv[]
+)
+{
+    double 🥈 ₇₉Au = 196.966570;
+    double 🥈 ₄₇Ag = 107.8682;
+    print("gold ⬚\n", ﹟F(₇₉Au));
+    print("silver ⬚\n", ﹟F(₄₇Ag));
+    Chronology c = ComputationalChronology();
+    __builtin_uint_t 🥇 autimes = c.ordinal()*₇₉Au;
+    print("imprint ⬚\n", ﹟d(autimes));
+    return 0;
+}
+
