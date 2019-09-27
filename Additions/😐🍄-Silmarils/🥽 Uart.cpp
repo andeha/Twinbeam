2083423122fd6b87394e445d09f31ab83787bcf1
Anders Hansson <anders.pm.hansson@gmail.com>
2019-09-27 13:32:50 +0200

    Added material.

---
+++ Additions/😐🍄-Silmarils/🥽 Uart.cpp
@@ -0,0 +1,30 @@
+/*  Uart.cpp | Symbols on-a-line. */
+
+#include <Twinbeam.h>
+
+/* Compile with: ./artᵐⁱᵖˢ.ksh Uart */
+
+#include <pic32rt/mips.hpp>
+#include <pic32rt/pic32mz.hpp>
+#include <pic32rt/pic32mzda.hpp>
+#include <pic32rt/stable.hpp>
+
+int
+main(
+  int argc,
+  const char * argv[]
+)
+{  InitMZDAStarterBoard();
+    print("Hello on the other side! 🖋😐\n");
+    /* See --<Additions>--<Fossilate.h>{WaitTerminal} for details on:
+      auto ping = ^(bool &stop) { print("."); };
+again:
+      char32_t uc;
+      int y = WaitTerminal(100, 10, ping, &uc);
+      if (y) { goto unagain; } else { 📨😐 } */
+    while (1) { ; }
+unagain:
+    print("timed out, possibly failed\n");
+    return 0;
+}
+
