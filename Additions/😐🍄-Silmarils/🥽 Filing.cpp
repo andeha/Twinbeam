9bad137c5b9b4a737b3e0d44ef5eaf7326b85395
Anders Hansson <anders.pm.hansson@gmail.com>
2019-10-14 10:47:29 +0200

    Added material.

---
+++ Additions/😐🍄-Silmarils/🥽 Filing.cpp
@@ -0,0 +1,27 @@
+/*  Filing.cpp | Big, b̲i̲g Rolodex. */
+
+#include <Twinbeam.h>
+#include <Additions/Additions.h>
+#include <Additions/Filesystem.hpp>
+
+/*  To compile: xcrun clang @ccargs_macᵧ² -o Filing ../../Source/llvmʳᵗ³.cpp  \
+  ../Additions.cpp '🥽 Filing.cpp' */
+
+#define const_questionable const
+
+int
+main(
+  int argc,
+  const char * argv[]
+)
+{  int 🥈 maxtetras=2048;
+    const_questionable char32_t * canonicalᵚ₁ = U"/tmp/zz.txt";
+    char32_t * canonicalᵚ₂ = (char32_t *)(U"/tmp/zz.txt");
+    char32_t * canonicalᵚ₃ = const_cast<char32_t *>(U"/tmp/zz.txt");
+    int tetras = UnicodesUntil𝟶𝚡𝟶𝟶𝟶𝟶𝘖𝘳𝖤𝖮𝖳(canonicalᵚ₂, maxtetras);
+    Unicodes ucs₁ { tetras, canonicalᵚ₂ };
+    if (FileSystemItemExists(ucs₁.tetras, ucs₁.unicodes)) { print("Exists\n"); } 
+    else { print("Does not exist\n"); }
+    return 0;
+}
+
