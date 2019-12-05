d62c143af96e1813be5e1a28c62e6e6b162609cf
Anders Hansson <anders.pm.hansson@gmail.com>
2019-12-05 10:09:55 +0100

    Improved exposition.

---
+++ Additions/😐🍄-Silmarils/🥽 Aliens.cpp
@@ -0,0 +1,22 @@
+/*  Aliens.cpp | 𝑺𝒑𝒂𝒄𝒆 in𝑣𝑎𝑑𝑒𝑟𝑠. */
+
+#include <Twinbeam.h>
+#include <Additions/Additions.h>
+
+/* 
+   🛸
+👾👾👾 
+⁂  ⁑  ﹡ *	   
+ 🤖 
+
+ */
+
+int 
+main(
+  int argc,
+  const char * argv[]
+)
+{
+    return 0;
+}
+
