72937bb83716bf72586bc50760f35be563eafbe5
Anders Hansson <anders.pm.hansson@gmail.com>
2019-12-21 12:48:14 +0100

    Improved exposition.

--- Additions/😐🍄-Silmarils/🥽 ✈️n🎲.cpp
+++ Additions/😐🍄-Silmarils/🥽 ✈️n🎲.cpp
@@ -2,12 +2,14 @@
 
 #include <Twinbeam.h>
 #include <Additions/Additions.h>
+#include <Additions/mathˢ.hpp>
 
 /* xcrun clang @ccargs_macᵧ² -o fliesNdice ../../Source/llvmʳᵗ³.cpp '🥽 ✈️n🎲.cpp' */
 
-struct Event { enum Act { tollgate } event; Chronology::Instant ts; };
-auto radio₁ = ^(void (^sometimes)(Event e, void * ctx)) { };
-auto radio₂ = ^(void (^sometimes)(Event e, void * ctx)) { };
+struct Ordinat { simd_tᵦ potency; }; /* ⬷ Normal and uniform. */
+struct Event { enum Act { tollgate } event; Chronology::Instant t; __builtin_int_t ordinat; };
+auto radio₁ = ^(const Ordinat& o, void (^sometimes)(Event e, void * ctx)) { };
+auto radio₂ = ^(const Ordinat& o, void (^sometimes)(Event e, void * ctx)) { };
 auto law = ^(Event e, void * ctx) {
   switch (e.event) {
   case Event::tollgate: print("tollgate\n"); break;
@@ -20,10 +22,10 @@ main(
   int argc, 
   const char * argv[]
 )
-{
+{  Ordinat oh¹ { 1.0, 1.0 };
     while (1) {
-      radio₁(^(Event e, void * ctx) { law(e,ctx); });
-      radio₂(^(Event e, void * ctx) { law(e,ctx); });
+      radio₁(oh¹, ^(Event e, void * ctx) { law(e,ctx); });
+      radio₂(oh¹, ^(Event e, void * ctx) { law(e,ctx); });
     }
     return 0;
 }
