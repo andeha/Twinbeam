0dabeaa9b8b0e40656875996ec7858fa7de6bf06
Anders Hansson <anders.pm.hansson@gmail.com>
2020-05-04 09:40:42 +0200

    Improved exposition.

--- Additions/mathˢ.hpp
+++ Additions/mathˢ.hpp
@@ -18,64 +18,7 @@ simd_tᵦ ℯ🔭²(simd_tᵦ 𝒙); simd_tᵦ half₋leaf logₑ(simd_tᵦ 𝒙
 
 #pragma mark 'With that special competitive edge' (Formerly in file named --<Additions>--<Watts.hpp>)
 
-MACRO simd_tᵦ 𝚙𝚘𝚠(simd_tᵦ 𝒃, simd_tᵦ 𝒙) { simd_tᵦ acc=logₑ(𝒃); 
-  acc = __builtin_simd_mulᵦ(𝒙,acc);
-  return ℯ🔭²(acc);
-} /* todo: `int pow(simd_tᵦ 𝒃, simd_tᵦ 𝒙, v2i64 &𝟷𝟶ᵐ, simd_tᵦ &significand)`. */
-
-MACRO simd_tᵦ logₓ(simd_tᵦ 𝒘, simd_tᵦ 𝒙) { /* logₓ w = ln w / ln x */
-  simd_tᵦ acc₁=logₑ(𝒘), acc₂=logₑ(𝒙);
-  simd_tᵦ acc = __builtin_simd_divᵦ(acc₁,acc₂);
-  return acc; }
-
-MACRO simd_tᵦ abs(simd_tᵦ 𝒙) { /* |x| */
-#ifdef NON₋SIMD
-  auto 𝒙₂ = 𝛽₋simd { .simd=𝒙 };
-  𝒙₂.doubles[1] = abs(𝒙₂.doubles[1]); 𝒙₂.doubles[0] = abs(𝒙₂.doubles[0]);
-  return 𝒙₂.simd;
-#elif defined __mips__ && !defined(NON₋SIMD)
-  simd_tᵦ 🥈 sign = simd_tᵦ { 31, 31 };
-  return __builtin_msa_bclr_d(𝒙,sign); /* Also: Shim { .mips=__builtin_msa_and_v(a₂.mips, b₂.mips) }.llvm */
-#elif defined __x86_64__ && !defined(NON₋SIMD) /*  Exercise~X: Validate |x| for ₋0.0 and ₊0.0. */
-  __uint128_t 𝗻𝗲𝗴ᵦ = Cherry::ltz₊(𝒙);
-  return Cherry::negate(𝒙,𝗻𝗲𝗴ᵦ);
-  /* union intel₋simd { double doubles[2]; simd_tᵦ simd; __m128d intelᵈ; __m128i intelⁱ; }; */
-#endif
-}
-
-MACRO simd_tᵦ sign(simd_tᵦ 𝒙) { /* Sometimes erroneously called `sgn`. */
-  simd_tᵦ 🥇 ₊𝟷=simd_initᵦ(1.0), ₋𝟷=simd_initᵦ(-1.0);
-  __uint128_t 𝗰𝗼𝗻𝗱ᵦ = Cherry::ltz₋(𝒙);
-  return Cherry::pick(₋𝟷, ₊𝟷, 𝗰𝗼𝗻𝗱ᵦ);
-}
-
-MACRO simd_tᵦ sin(simd_tᵦ 𝒙) { simd_tᵦ s,c; sincos(𝒙,s,c); return s; }
-MACRO simd_tᵦ cos(simd_tᵦ 𝒙) { simd_tᵦ s,c; sincos(𝒙,s,c); return c; }
-
-MACRO simd_tᵦ asin(simd_tᵦ 𝒙) { /* asin(x) = 2 arctan(x/(1 + √(1-x²))), where x≤1 leads to complex later. */
-  simd_tᵦ 🥇 𝟷=simd_initᵦ(1.0), 𝟸=simd_initᵦ(2.0);
-  simd_tᵦ x² = __builtin_simd_mulᵦ(𝒙,𝒙);
-  simd_tᵦ 𝟷⁻x² = __builtin_simd_subᵦ(𝟷,x²);
-  simd_tᵦ ₍𝟷⁻x²₎⁰⁵ = __builtin_simd_sqrtᵦ(𝟷⁻x²);
-  simd_tᵦ denom = __builtin_simd_addᵦ(𝟷, ₍𝟷⁻x²₎⁰⁵);
-  simd_tᵦ param = __builtin_simd_divᵦ(𝒙, denom);
-  simd_tᵦ 𝖺𝗍𝖺𝗇 = arctan(param);
-  return __builtin_simd_mulᵦ(𝟸, 𝖺𝗍𝖺𝗇);
-} /* Also: arcsin(x) = π/2 - sqrt(1 - x)(a₀ + a₁*x + a₂*x² + a₃*x³),
-   a₀=1.5707288, a₁=-0.2121144, a₂=0.0742610, a₃=-0.0187293 */
-
-MACRO simd_tᵦ acos(simd_tᵦ 𝒙) { /* acos(x) = 2 arctan(√(1-x²) / (1+x)) */
-  simd_tᵦ 🥇 𝟷=simd_initᵦ(1.0), 𝟸=simd_initᵦ(2.0);
-  simd_tᵦ x² = __builtin_simd_mulᵦ(𝒙,𝒙);
-  simd_tᵦ 𝟷⁻x² = __builtin_simd_subᵦ(𝟷,x²);
-  simd_tᵦ num = __builtin_simd_sqrtᵦ(𝟷⁻x²);
-  simd_tᵦ denom = __builtin_simd_addᵦ(𝟷,𝒙);
-  simd_tᵦ param = __builtin_simd_divᵦ(num, denom);
-  simd_tᵦ 𝖺𝗍𝖺𝗇 = arctan(param);
-  return __builtin_simd_mulᵦ(𝟸, 𝖺𝗍𝖺𝗇);
-} /* See also --<khinchin.cpp>: double khinchin_arccos(double x) { return π/2 - khinchin_arcsin(x); } */
-
-MACRO simd_tᵦ frac(simd_tᵦ 𝒙) { simd_tᵦ 🥇 𝟷=simd_initᵦ(1.0); return mod(𝒙,𝟷); }
+/* ... */
 
 #pragma mark 'Primititives for a single `double` type' (--<Additions>--<Stirlings.hpp>)
 
