17f16ae7d7fc37b009a06259055237fb9cc510c8
Anders Hansson <anders.pm.hansson@gmail.com>
2020-06-02 20:18:10 +0200

    Improved exposition.

--- Additions/Font⋮⋮⋮.h
+++ Additions/Font⋮⋮⋮.h
@@ -1,17 +1,17 @@
-/*  Rasterizer.h | Additions. */
+/*  Rasterizer.h | Pixelation given an .otf-file font definition. */
 
 #ifndef __RASTERIZER_H
 #define __RASTERIZER_H
 
 #include <Twinbeam.h>
 
-struct FontRasterizer {
+struct Pixelation {
     
-    FontRasterizer(int horizDPI, int vertDPI);
+    Pixelation(int horiz₋dpi, int vert₋dpi);
     
-    ~FontRasterizer();
+    ~Pixelation();
     
-    typedef struct BoundingBox { signed long xMin, yMin, xMax, yMax; } BBox;
+    typedef struct Boundingbox { signed long xMin, yMin, xMax, yMax; } BBox;
     
     int renderMonocrome(int width, int height, Memoryview otfFont, char32_t 
       (^text)(int index), void (^bitmap)(int width, int rows, uint8_t *buffer, 
@@ -22,9 +22,8 @@ struct FontRasterizer {
       int pitch, int horzKern));
     
     int renderOutline(int width, int height, Memoryview otfFont, char32_t 
-      (^text)(int index), void (^outline)(BoundingBox boundingBox, int 
-      horzKern));
+      (^text)(int index), void (^outline)(Boundingbox box, int horzKern));
     
 😐;
 
-#endif /* An example font is defined in Fig~302, [Knuth, A Potpourri of Puzzles]. */
+#endif /* See Fig~302 in [Knuth, A Potpourri of Puzzles] for an example font. */
