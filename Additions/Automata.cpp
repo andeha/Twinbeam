2b184044d49a34da1fe7a2c1937575449a678f62
Anders Hansson <anders.pm.hansson@gmail.com>
2021-03-20 20:46:01 +0100

    Improved exposition.

--- Additions/Automata.cpp
+++ Additions/Automata.cpp
@@ -1,26 +1,27 @@
-/*  Automata.cpp | Input conversion, tokenization and parsing. */
+/*  Automata.cpp | input conversion, tokenization and parsing. */
 
 #include <Twinbeam.h>
 #include <Additions/Additions.h>
 
 Opt<__builtin_int_t>
-CastᵗˣᵗToInt(
+CastTˣᵗToInt(
   CastToIntOpinion (^feeder)(unsigned short &digit)
 )
 { __builtin_int_t val = 0; __block unsigned short zero_to_nine; int sgn = 1;
-    while (1) {
-       CastToIntOpinion opinion = feeder(zero_to_nine);
-       switch (opinion) {
-         case CastToIntOpinion::accept: val *= 10; val += sgn*zero_to_nine; break;
-         case CastToIntOpinion::rejecting: continue;
-         case CastToIntOpinion::negate: sgn *= -1; break;
-         case CastToIntOpinion::commit: return Opt<__builtin_int_t>(val);
-         case CastToIntOpinion::annul: return Opt<__builtin_int_t>::no();
-        }
-     }
+   while (1) {
+      CastToIntOpinion opinion = feeder(zero_to_nine);
+      /* In C++20: 'using enum CastToIntOpinion;' */
+      switch (opinion) {
+       case CastToIntOpinion::accept: val *= 10; val += sgn*zero_to_nine; break;
+       case CastToIntOpinion::rejecting: continue;
+       case CastToIntOpinion::negate: sgn *= -1; break;
+       case CastToIntOpinion::commit: return Opt<__builtin_int_t>(val);
+       case CastToIntOpinion::annul: return Opt<__builtin_int_t>::no();
+      }
+   }
 }
 
-const int MAX_TOKEN = 8192;
+int 🥈ᵢ MAX_TOKEN = 8192;
 
 int
 ReadUtf8(
@@ -28,16 +29,16 @@ ReadUtf8(
   Inputcontrol (^line)(char * line)
 )
 { char utf8bytes[MAX_TOKEN]; int pos = 0;
-    while (1) { if (pos >= MAX_TOKEN) return -1;
-       Readlineopinion hypothesis = feeder(*(utf8bytes + pos));
-       switch (hypothesis) {
-          case Readlineopinion::accept: pos++; break;
-          case Readlineopinion::rejecting: continue;
-          case Readlineopinion::commit: if (line(utf8bytes) ==
-            Inputcontrol::quit) return 0; pos=0; break;
-          case Readlineopinion::quit: return -1;
-        }
-    }
+   while (1) { if (pos >= MAX_TOKEN) return -1;
+      Readlineopinion hypothesis = feeder(*(utf8bytes + pos));
+      switch (hypothesis) {
+       case Readlineopinion::accept: pos++; break;
+       case Readlineopinion::rejecting: continue;
+       case Readlineopinion::commit: if (line(utf8bytes) ==
+        Inputcontrol::quit) return 0; pos=0; break;
+       case Readlineopinion::quit: return -1;
+      }
+   }
 }
 
 int
@@ -82,48 +83,75 @@ Tokenize(
     }
 }
 
-#pragma mark - Language Translation
+#pragma mark - Language translation: 'Token is a noun'
 
 int
 TokenizeUtf8OrUnicode(
   Encoding encoding,
-  Memoryview content,
+  memoryview content,
   __builtin_int_t& beam,
-  void (^/*several-*/character)(char32_t unicode, __builtin_int_t byteOffset, bool& stop)
+  void (^none𝘖rSeveral)(char32_t unicode, __builtin_int_t byteOffset, bool& stop)
 )
-{
-    __builtin_int_t bytes = content.byteCount, i = beam;
-    SemanticPointer<void *> base = content.region->start();
-    while (i < bytes) {
-        SemanticPointer<void *> p = content.region->relative(i + content.bytesOffset, base);
-        __builtin_int_t charBytes = 4; bool stop = false;
-        if (encoding == Encoding::utf8) {
-            uint8_t * c = (uint8_t *)p.pointer;
-            charBytes = Utf8Followers(*c) + 1;
-            if (charBytes == -1) return -1; /* Possibly non-utf8 byte. */
-            char32_t unicode = Utf8ToUnicode(c, charBytes);
-            if (unicode == 0xFFFE || unicode == 0xFFFF) { return -2; }
-            character(unicode, i, stop);
-        } else { character(*(char32_t *)(p.pointer), i, stop); }
-        i += charBytes; beam = i;
-        if (stop) break;
+{    __builtin_int_t bytes=content.bytecount, i=beam; Bits bits { content.scatter };
+    while (i < bytes) { /* disjunct/sediment/segments */
+       __builtin_int_t charBytes=4; bool stop=false;
+       if (encoding == Encoding::utf8) {
+          char8_t c = bits.operator[](i);
+          charBytes = Utf8Followers(c) + 1;
+          if (charBytes == -1) return -1; /* Possibly non-utf8 byte. */
+          char32_t unicode = Utf8ToUnicode(&c,charBytes);
+          if (unicode == 0xFFFE || unicode == 0xFFFF) { return -2; }
+          none𝘖rSeveral(unicode,i,stop);
+       } else {
+          uint32_t unicode = bits.mips𝟷ᵈ(i, 0, Sentinel::bound, bytes<<2);
+          none𝘖rSeveral(unicode,i,stop); }
+       i += charBytes; beam=i;
+       if (stop) break;
     }
     return 0;
 }
 
-#pragma mark - Time
+int TokenizeUtf8ToUnicode(uint8_t * material, short bytes, 
+  void (^none𝘖rSeveral)(__builtin_int_t byteOffset, char32_t unicode, 
+    __builtin_int_t utf8bytes, bool& stop)
+)
+{  __builtin_int_t i=0; bool stop=false; while (1) {
+     if (i > bytes) { return -1; }
+     if (i == bytes) { goto finished; }
+     char8_t c = *(material + i);
+     __builtin_int_t charbytes = 1 + Utf8Followers(c);
+     if (charbytes == -1) return -2; /* Possibly non-utf8 byte. */
+     char32_t unicode = Utf8ToUnicode(&c,charbytes);
+     if (unicode == 0xFFFE || unicode == 0xFFFF) { return -3; }
+     none𝘖rSeveral(i,unicode,charbytes,stop);
+     if (stop) { return -4; }
+     i += charbytes;
+  }
+finished:
+  return 0;
+} /* ⬷ called from --<Additions>--<Reflect.cpp>. */
 
-Opt<Chronology::Instant>
+#pragma mark - Incremental vs. toggling 
+
+int Chess(const char * 𝟽alt𝟾₋bitstring, 
+  void (^move)(int fromrow, int fromcol, int torow, int tocol)
+) { return 0; }
+
+/* lon and lat; unicode csv, money/optcurrency */
+
+#pragma mark - Times and datestamps
+
+Opt<Chronology::instant>
 TS(
   Encoding encoding,
   Chronology chronology,
-  Memoryview datetime
+  memoryview datetime
 ) NEVERBLURTS
-{ /* …or 'Find chronology𝛥 and recursively parse l and r'. */
+{ /* …or 'find chronology𝛥 and recursively parse l and r'. */
     
     𝑓𝑙𝑢𝑐𝑡𝑢𝑎𝑛𝑡 enum class State { init, y1_2, y2_3, y3_4, y_hyphen, hyphen_M,
-        M1_2, M2_hyphen, hyphen_d1, d1_2, date_time, h1_2, h2_colon, colon_m1,
-        m1_2, m2_colon, colon_s1, s1_2, s2_optfrac, dot_frac, time, error
+      M1_2, M2_hyphen, hyphen_d1, d1_2, date_time, h1_2, h2_colon, colon_m1,
+      m1_2, m2_colon, colon_s1, s1_2, s2_optfrac, dot_frac, time, error
     } state = State::init;
     
     auto digit = ^(char32_t u) { return (0x30 <= u && u < 0x40); };
@@ -136,17 +164,19 @@ TS(
     auto ws𝘖𝘳EOT = ^(char32_t u) { return u == END_OF_TRANSMISSION || whitespace(u); };
     
     uint32_t 🥈 iⁿbase₂[] = { 0, /* 0, 1/10, 1/100, ..., 1/10⁻32 in UQ32. */
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 }; /* ⬷ See --<🥽 Alloca.cpp>. */
+      0x19999999,0x28f5c28,0x418937,0x68db8,0xa7c5,0x10c6,0x1ad,0x2a,0x4,0,
+      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 }; /* ⬷ see --<🥽 Alloca.cpp>. */
     auto augment = ^(short 𝟶to𝟿, unsigned 𝟷𝟶⁻ˣ, uint32_t frac) { return (𝟶to𝟿*frac)*iⁿbase₂[𝟷𝟶⁻ˣ]; };
     /* 1/10 = 1/16 + 1/32 + 1/256 + 1/512 enum Rounding { RoundNearestUnder, 
       RoundNearestOver, RoundNearestErrorSquared }; */
     uint32_t 𝑓𝑙𝑢𝑐𝑡𝑢𝑎𝑛𝑡 acc=0; /* E․𝘨 0.101₂ = 1×1/2 + 0×1/4 + 1×1/8 = 5/8․ */
     unsigned 𝑓𝑙𝑢𝑐𝑡𝑢𝑎𝑛𝑡 𝟷𝟶⁻ˣ=1; int32_t 𝑓𝑙𝑢𝑐𝑡𝑢𝑎𝑛𝑡 y=0, M=0, d=0, h=0, m=0, s=0;
     /* Also: ∑ f(k)/10⁻k = ∑f₁(k)/2⁻k - ∑f₂(k)/2⁻k */
+    /* ...as a 'reduction': NP = P*NP [See also Abramowitz and Stegun]. */
     
     🧵(Error) {
-      case Error: return Opt<Chronology::Instant>::no();
-    } /* ⬷ Note that we're exiting from inside a block using `confess` ⤐ */
+      case Error: return Opt<Chronology::instant>::no();
+    } /* ⬷ Note that we're exiting from inside a block using `confess`. ⤐ */
     
     auto process = ^(char32_t u) {
         switch (state) {
@@ -245,13 +275,13 @@ TS(
        beam.unicodeLook(0, BinaryChoiceToRight); */
      if (TokenizeUtf8OrUnicode(encoding, datetime, beam, ^(char32_t unicode,
        __builtin_int_t byteOffset, bool& stop) { process(unicode); }))
-     { return Opt<Chronology::Instant>::no(); }
+     { return Opt<Chronology::instant>::no(); }
     
     if (state != State::time && state != State::s2_optfrac)
-        return Opt<Chronology::Instant>::no();
+        return Opt<Chronology::instant>::no();
     
     int32_t t[6] = { y, M, d, h, m, s };
     
-    return chronology.timestamp(t,acc);
+    return chronology.integers₋encode(t,acc);
 }
 
