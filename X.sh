b5c048cc4318fdd6d95fe6813cfbdd64de92025b
Anders Hansson <anders.pm.hansson@gmail.com>
2021-04-12 10:41:30 +0200

    Improved exposition.

--- X.sh
+++ X.sh
@@ -3,6 +3,8 @@ ninja -C Source -f build_macos.ninja                                || exit 1
 # lipo -create .. as of Xcode 12.2 only for arm and intel, not mips.
 ninja -C Additions -f bld_mac.ninja                                 || exit 1
 ninja -C Additions -f bld_mz.ninja                                  || exit 1
+ninja -C Additions -f seismic-mac.ninja                             || exit 1
+ninja -C Additions -f seismic-mz.ninja                              || exit 1
 ninja -C Additions -f bldº_mz.ninja                                 || exit 1
 ninja -C Additions -f bldº_mac.ninja                                || exit 1
 # ninja -C macOS -f :work:.ninja.ninja /work/.ninja.ninja           || exit 1
@@ -21,13 +23,15 @@ ninja -C Examples/radio
 # ninja -C Examples/attokernel
 # ninja -C Examples/😐/
 ninja -C Examples/sdhc
+ninja -C macOS -f harness-sv-1.ninja                                || exit 1
 ninja -C macOS -f harness-1.ninja                                   || exit 1
 ninja -C macOS -f harness-2.ninja                                   || exit 1
 ninja -C macOS -f harnessᵃᵈᵈˢ.ninja                                 || exit 1
 ninja -C macOS -f harnessᵃᵘˣ.ninja                                  || exit 1
-ninja -C Additions/History -f seismic_mac.ninja                     || exit 1
-ninja -C Additions/History -f seismic_mz.ninja                      || exit 1
+ninja -C Additions -f bld-annex.ninja                               || exit 1
 # ninja -C Additions/Storage -f dgate_mac.ninja                       || exit 1
 # ninja -C Additions/Storage -f dgate_mz.ninja                        || exit 1
 ninja -C ../helixsh build_macos.ninja                               || exit 1
 ninja -C ../vt99 build_macos.ninja
+
+
