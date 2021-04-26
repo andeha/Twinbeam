0b47384e4a5a685982a8ea50bb5f6f52bdf030f6
Anders Hansson <anders.pm.hansson@gmail.com>
2021-04-26 07:57:32 +0200

    Improved exposition.

--- Source/sequent.hpp
+++ Source/sequent.hpp
@@ -2,22 +2,18 @@
 
 struct sequent { Fixpoint::Q6463 detail; uint8_t rl₋decimal₋count; int valid; };
 struct cplx₋sequent { sequent magnitude, rad₋angle; };
-sequent operator+(const sequent& x₁, const sequent& x₂);
-sequent operator-(const sequent& x₁, const sequent& x₂);
-sequent operator/(const sequent& x₁, const sequent& x₂);
-sequent operator*(const sequent& x₁, const sequent& x₂);
 sequent operator+(sequent x₁, sequent x₂);
 sequent operator-(sequent x₁, sequent x₂);
 sequent operator*(sequent x₁, sequent x₂);
 sequent operator/(sequent x₁, sequent x₂);
 sequent product₋abelian(); /* ⬷ a․𝘬․a '1'. */
 sequent accumulative₋zero(); /* ⬷ a․𝘬․a '0'. */
-sequent negative₋inf();
-sequent operator-(const sequent& x, int);
+sequent negative₋inf(); /* ⬷ a․𝘬․a -∞. */
+sequent operator-(sequent x);
 sequent n₋sqrt(sequent n, sequent ℝ);
-sequent sincos(sequent Θ, sequent * s, sequent * c);
-sequent log₂(sequent ℝ);
-sequent lnΓ(cplx₋sequent z);
+void sincos(sequent Θ, sequent * S, sequent * C);
+sequent log₃(sequent ℝ);
+sequent lnΓ(cplx₋sequent ℂ);
 sequent modulo(sequent x, sequent y);
 #if defined(𝟷𝟸𝟾₋bit₋integers) && defined(IEEE754₋ARITHMETICS₋INSIDE)
 sequent operator "" _följd(long double ℝ);
@@ -33,7 +29,9 @@ int Newton(computational f, computational f₋prime, sequent * x₀,
  void (^ping)(Newtoncontrol &ctrl));
 void Khinchin(sequent z, sequent (^A)(sequent k, sequent z), 
  sequent (^B)(sequent k, sequent x), int count, sequent &acc);
-void print₋sequent(const sequent& 𝕏, void (^digits)(bool neg, int 𝟷𝟶ˣ, 
- 𝟽bit₋text 𝟶to𝟿s), void (^zero)(), void (^neginf)(), void (^valid)());
+void print₋sequent(sequent 𝕏, void (^digits)(bool neg, 𝟽bit₋text 𝟶to𝟿s, 
+ 𝟽bit₋text ᐧ𝟶to𝟿s), void (^zero)(), void (^neginf)(), void (^nonvalid)());
+int round₋𝟸⁻ⁱ₋decimals(short unsigned upto𝟼𝟺, char ᐧ𝟶to𝟿s[], sequent * out);
+/* ⬷ converts .d0d1...dk-1 where 0 <= k <= 17. TeX 102 §. */
 
 
