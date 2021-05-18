e93b12e794119980eb37666baa0e0e50dcb82ac5
Anders Hansson <anders.pm.hansson@gmail.com>
2021-05-18 18:13:46 +0200

    Improved exposition.

---
+++ Source/Twinbeam.h
@@ -0,0 +1,6 @@
+#include <../Apps/Source/fifo.h>
+#include <../Apps/Source/sequent.hpp>
+#include <../Apps/Source/sequent–2.hpp>
+#include <../Apps/Source/timeserie.hpp>
+#include <../Apps/Source/timeserie₂.hpp>
+
