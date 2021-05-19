5c21b3616d3506bb06e301f4ae779ccc33ab8028
Anders Hansson <anders.pm.hansson@gmail.com>
2021-05-19 05:36:07 +0200

    Improved exposition.

--- Source/Twinbeam.h
+++ Source/Twinbeam.h
@@ -1,6 +1,1 @@
-#include <../Apps/Source/fifo.h>
-#include <../Apps/Source/sequent.hpp>
-#include <../Apps/Source/sequent–2.hpp>
-#include <../Apps/Source/timeserie.hpp>
-#include <../Apps/Source/timeserie₂.hpp>
-
+#include <../Apps/Additions/Typecase.h>
