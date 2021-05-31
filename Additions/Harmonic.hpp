1d83bc8cefa5d8cc4c3b8483bca420e862720bda
Anders Hansson <anders.pm.hansson@gmail.com>
2021-05-31 12:59:35 +0200

    Improved exposition.

--- Additions/Harmonic.hpp
+++ Additions/Harmonic.hpp
@@ -1,4 +1,4 @@
-/* Harmonic.hpp - Flavored Rationals. */
+/*  Harmonic.hpp - flavored rationals. */
 
 /* International Jazz-festival day: 'Sixth of June'. */
 
@@ -6,13 +6,11 @@
 
 /* ⟪ 1, 2, 3, 4, 5, 6, ♭7, 8/1, 9/2, 10/3, 11/4, 12/5, 13/6 ⟫ */
 
-#include <Additions/Fractions.hpp>
-
 #pragma mark - Exercise: Tuning G-string
 
 enum Intervalᴴ { quinte, terz, quarte, sexte, septime, oktave, sekunde, dezime };
 enum Alterificate { flat, natural, sharp };
-enum ArtisticDirectionᴴ { }; /* Harmonically speaking, actually. */
+enum ArtisticDirectionᴴ { Improved }; /* Harmonically speaking actually. */
 
 namespace ᴴInterval { /* Compute 𝞮 when 88 keys and Q4.12 compared to when in `x86_fp80`. */
 constexpr auto purePerfectFifth        = Tuple<float, float>(0.75000, 1.50000); /* 3/2=1+1/2, 3/4 */
@@ -23,39 +21,53 @@ constexpr auto wholestep               = Tuple<float, float>(1.12500, 0.56250); 
 constexpr auto halfstep                = Tuple<float, float>(1.06667, 0.53333); /* 16/15=1+1/15, 16/30 */
 constexpr auto leadingMinorThird       = Tuple<float, float>(1.20000, 0.60000); /* 6/5=1+1/5, 3/5 */
 constexpr auto augmentedFourth         = Tuple<float, float>(1.40625, 0.70313); /* 45/32=1+13/32, 45/64 */
-constexpr auto octave                  = Tuple<float, float>(2.00000, 0.50000); /* 2=😐+😐/😐, 1/2 */
+constexpr auto ieeryoctave             = Tuple<float, float>(2.00000, 0.50000); /* 2=😐+😐/😐, 1/2 */
 constexpr auto itchysMinorsixth        = Tuple<float, float>(1.60000, 0.80000); /* 8/5=1+3/5 4/5 */
 constexpr auto sizzlingMinorseventh    = Tuple<float, float>(1.80000, 0.90000); /* 9/5=1+4/5, 9/10 */
 constexpr auto concernedMajorseventh   = Tuple<float, float>(1.87500, 0.93750); /* 15/8=1+7/8, 15/16 */
 /* Ptolemy: 1/1 16/15 9/8 6/5 5/4 4/3 45/32 3/2 8/5 5/3 9/5 15/8 2/1 */
 /* Scandianvian delight: ⟪ 1 ♯2 ♭3, 4, 5, ♭6, 7 ⟫ */
 /* constexpr auto scandinavianseventh = Tuple<float, float>(19/17, 17/34); */
+enum { alt, sopran, bas, tenor };
+enum { attack, decay, contra₋decay, hesitat, controvers, sustain, release };
+enum Accent { mazurka, marsch, foxtrot };
+int Metronome(Accent t);
 }
 
-/* Instrumentally_ */ FOCAL
-inline 
+/* Instrumental */ FOCAL
+inline
 Tuple<float, float>
 Interval(
   Intervalᴴ i,
   Alterificate a,
-  float cpsOrHz /* unison */,
+  float cps𝘖rHz /* unison */,
   float 𝞮
 )
 {
-    Tuple<float, float> 🥈ᵢ resolvation = {
-      { ᴴInterval::augmentedFourth, ᴴInterval::purePerfectFifth, ᴴInterval::itchysMinorsixth },
-      { ᴴInterval::leadingMinorThird, ᴴInterval::consonantMajorthird, ᴴInterval::compactPerfectfourth },
-      { ᴴInterval::consonantMajorthird, ᴴInterval::compactPerfectfourth, ᴴInterval::augmentedFourth },
-      { ᴴInterval::itchysMinorsixth, ᴴInterval::contemplativeMajorsixth, ᴴInterval::sizzlingMinorseventh /* 🌶⤣ */ },
-      { ᴴInterval::sizzlingMinorseventh, ᴴInterval::concernedMajorseventh, ᴴInterval::octave },
-      { ᴴInterval::concernedMajorseventh, ᴴInterval::octave, ᴴInterval::halfstep },
-      { ᴴInterval::halfstep, ᴴInterval::wholestep, ᴴInterval::leadingMinorThird },
-      { (2+𝞮)*ᴴInterval::itchysMinorsixth, (2+𝞮)*ᴴInterval::contemplativeMajorsixth, (2+𝞮)*ᴴInterval::sizzlingMinorseventh }
+    using ᴴInterval;
+    Tuple<float, float> 🥈 resolvation = {
+      { augmentedFourth,       purePerfectFifth,        itchysMinorsixth     },
+      { leadingMinorThird,     consonantMajorthird,     compactPerfectfourth },
+      { consonantMajorthird,   compactPerfectfourth,    augmentedFourth      },
+      { itchysMinorsixth,      contemplativeMajorsixth, sizzlingMinorseventh },
+      { sizzlingMinorseventh,  concernedMajorseventh,   octave               },
+      { concernedMajorseventh, ieeryoctave,             halfstep             },
+      { halfstep,              wholestep,               leadingMinorThird    },
+      { (2+𝞮)*itchysMinorsixth, (2+𝞮)*contemplativeMajorsixth, (2+𝞮)*sizzlingMinorseventh }
     };
-    return resolvation[(int)i][((int)a)]*cpsOrHz;
+    return resolvation[(int)i][((int)a)]*cps𝘖rHz;
 }
 
-inline float AngularFrequency(float cpsOrHz) { return 2*Naturals::π*cpsOrHz; } /* 𝜔 = 2*π*f [rad/s]. */
+inline 
+float 
+AngularFrequency(
+  float cpsOrHz,
+  bool gothic
+)
+{
+    return 2*Naturals::π*cpsOrHz; 
+} /* 𝜔 = 2*π*f (rad/s); when gothic, E is lowered to E♭ ±2-3 cent.
+  Hypothetically depending on air. */
 
 Intervalᴴ Flagolett(Intervalᴴ point); /* Do not consider `Abalone`. */
 
