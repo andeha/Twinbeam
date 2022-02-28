143cdc1252767fcbb047db876467f1483d5090e7
Anders Hansson <anders.pm.hansson@gmail.com>
2022-02-28 15:29:00 +0100

    Improved exposition.

--- X.sh
+++ X.sh
@@ -1,8 +1,26 @@
-ninja -C Source -f build_macos.ninja                                || exit 1
+#!/usr/bin/env /bin/zsh
+builtin typeset BUNDLEID='ABCD1234'
+builtin typeset TEAMID='4321DCBA'
+builtin typeset signcode
+while [[ $# -gt 0 ]]; do
+  case $1 in
+   -s | --codesign)
+     shift
+     signcode="yes"
+     ;;
+  esac
+done
+# ⬷ alternatively prompt> schema 'TEAMID=$S and BUNDLEID=$S'.
+ninja -C Source -f build_armmac.ninja                               || exit 1
 ninja -C Source -f build_pic32mz.ninja                              || exit 1
-ninja -C Source -f build_pic32mm.ninja
-# ⬷ see progress on --<🥽 assemble.cpp> and --<􀐒 retrospect.c>. 
-# lipo -create .. as of Xcode 12.2 only for arm and intel, not mips.
+ninja -C Source -f build_pic32mm.ninja                              || exit 1
+ninja -C Source -f build_intcmac.ninja                              || exit 1
+lipo -create -output libTwinbeam_macos.a libTwinbeam_x86.a               \
+ libTwinbeam_arm.a                                                  || exit 1
+if [[ -n "$signcode" ]]; then
+  codesign -s ${TEAMID} -f -o runtime --timestamp -i ${BUNDLEID} libTwinbeam_macos.a
+fi
+# ⬷ lipo -create ... as of Xcode 13.2 only for arm and intel and not mips.
 ninja -C Additions -f bld_mac.ninja                                 || exit 1
 ninja -C Additions -f bld_mz.ninja                                  || exit 1
 ninja -C Additions -f seismic-mac.ninja                             || exit 1
@@ -13,6 +31,7 @@ ninja -C Additions -f bld-o-mac.ninja                               || exit 1
 ninja -C Bootloader -f build-mm.ninja                               || exit 1
 ninja -C Bootloader -f build-mzda.ninja                             || exit 1
 ninja -C Sprinkle/pdb                                               || exit 1
+# ⬷ see progress on --<🥽 assemble.cpp> and --<􀐒 retrospect.c>.
 ninja -C Examples/dbgout
 ninja -C Examples/ledflash
 ninja -C Examples/helloworld
@@ -38,4 +57,6 @@ ninja -C Additions -f bld-annex.ninja                               || exit 1
 # ninja -C Additions/grayzones -f miller_mac.ninja                    || exit 1
 ninja -C ../helixsh build_macos.ninja                               || exit 1
 ninja -C ../Minimum -f build.ninja
-
+if [[ -n "$signcode" ]]; then
+  codesign -f -s ${TEAMID} -o runtime --timestamp -i {BUNDLEID} ../Minimum/Minimum.app
+fi
