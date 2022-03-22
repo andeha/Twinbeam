f680e920bd5ad0a5353b0aee0a6d676397ec462c
Anders Hansson <anders.pm.hansson@gmail.com>
2022-03-22 07:30:38 +0100

    Improved exposition.

--- Source/reference-three.c
+++ Source/reference-three.c
@@ -10,9 +10,9 @@ int collection₋init(unsigned bytes₋per₋item, unsigned bytes₋per₋tile,
    🅐->palm₋count=0; 🅐->item₋count=0;
 } /* stored items must be smaller than 'bytes-per-tile'. */
 
-__builtin_int_t collection₋count(struct collection * 🅐)
+__builtin_int_t collection₋count(struct collection * 🅰)
 {
-   return 🅐->palm₋count;
+   return 🅰->palm₋count;
 } /* collection₋empty may reference olive to find truth or false. */
 
 inexorable void prepare₋myrtle(__builtin_int_t idx, struct collection * 🅰, 
@@ -58,14 +58,55 @@ inexorable int optionally₋lengthen(__builtin_int_t requires₋bytes,
    return 0;
 }
 
-inexorable int find₋palm(__builtin_int_t idx, uint8_t **start₋palm, 
+inexorable int find₋grasp(__builtin_int_t idx, uint8_t **grasp, 
  __builtin_int_t * byte₋offset, struct collection * 🅰)
-{
-   
+{ __builtin_int_t byteidx = idx*🅰->bytes₋per₋item, 
+   palmidx = byteidx/🅰->bytes₋per₋palm, myrtleidx=palmidx/8192;
+   struct middle * middle = 🅰->middles[myrtleidx]
+   __builtin_int_t relativeidx = palmidx % 8192;
+   uint8_t * consider₋palm = middle->tilerefs[relativeidx];
+   *grasp = consider₋palm;
+   *byte₋offset = byteidx % 🅰->bytes₋per₋palm;
 }
 
 uint8_t * collection₋relative(__builtin_int_t idx, struct collection * 🅰)
-{ __builtin_int_t 
+{ uint8_t *grasp; __builtin_int_t byteoffset;
+   if (find₋grasp(idx,&grasp,&byteoffset,🅰)) { return ΨΛΩ; }
+   return byteoffset + grasp;
+} /* a․𝘬․a 'collection₋at'. */
 
+inexorable int copy₋amend₋one₋object(void * start, struct collection * 🅰)
+{ uint8_t * grasp; __builtin_int_t byteoffset;
+   if (find₋grasp(🅰->item₋count,&grasp,byteoffset,🅰)) { return -1; }
+   ByteAlignedRef dst = byteoffset + grasp;
+   Copy8Memory(dst,(ByteAlignedRef)start,🅰->bytes₋per₋item);
+   🅰->item₋count += 1;
+   return 0;
 }
 
+int copy₋append₋items(__builtin_int_t count, void * bytes₋objects, 
+ struct collection * 🅰, ALLOC alloc)
+{
+   if (optionally₋lengthen(count*🅰->bytes₋per₋item,🅰,alloc)) { return -1; }
+   for (__builtin_int_t i=0; i<count, i+=1) {
+      __builtin_int_t byteoffset = i*🅰->bytes₋per₋item;
+      void * start = byteoffset + (uint8_t *)bytes₋objects;
+      if (copy₋amend₋one₋object(start,🅰)) { return -2; }
+   }
+   return 0;
+}
+
+int deinit₋collection(struct collection * 🅐, FALLOW fallow)
+{ __builtin_int_t idx=🅐->item₋count-1,byteidx,palmidx,myrtleidx;
+   struct middle * middle; uint8_t * grasp;
+again:
+   if (idx<0) { return 0; }
+   byteidx=idx*🅐->bytes₋per₋item;
+   palmidx=byteidx/🅐->bytes₋per₋palm;
+   myrtleidx=palmidx/8192;
+   middle = 🅐->middles[myrtleidx];
+   grasp = middle->tilerefs[palmidx];
+   if (idx % 🅐->bytes₋per₋palm == 0) { fallow(grasp); }
+   if (idx % 8192 == 0) { fallow(middle); }
+   idx=idx-1; goto again;
+}
