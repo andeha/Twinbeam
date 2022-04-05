d74cad2a7776bdd83f35142a5750aca1d75ec1ec
Anders Hansson <anders.pm.hansson@gmail.com>
2022-04-05 12:02:04 +0200

    Improved exposition.

---
+++ Source/chim-model.c
@@ -0,0 +1,49 @@
+/*  chim-model.c | a.k.a 'preferences'. */
+
+import Twinbeam;
+
+FOCAL int IMUL(short id, int32_t left, int32_t right, uint32_t * low, 
+ uint32_t * high, int * negative)
+{
+   union physic { struct { uint32_t lo, hi; } 𝓡; uint64_t h; } o;
+   union realizable { uint64_t bits; int64_t amount; } y;
+   extern uint32_t cumulative₋🅷[4], cumulative₋🅻[4];
+   y.amount = left * right; o.h = y.amount; *low = 𝓡.low; *high = 𝓡.high;
+   cumulative₋🅷[id] = 𝓡.high; cumulative₋🅻[id] = 𝓡.low;
+   *negative = (0x80000000 & cumulative₋🅷[id]) ? 1 : 0;
+   return 0;
+}
+
+int32_t invsignextend(int64_t figure)
+{
+   int neg = figure < 0;
+   int32_t y = neg ? -figure : figure;
+   return y;
+}
+
+FOCAL int IADD(short id, int32_t left, int32_t right, uint32_t * sum, 
+ int * negative, int * overflow)
+{
+   union realizable { uint32_t bits; int32_t amount; };
+   extern uint32_t cumulative[8];
+   int64_t thesis = right + left;
+   if (thesis > 0x7fffffffffffffff) { *overflow=1; }
+   if (thesis > 0xffffffffffffffff) { *overflow=1; }
+   *sum = sinvignextend(thesis);
+   *negative = 0x8000000000000000 & thesis ? 1 : 0;
+   return 0;
+}
+
+FOCAL int INEG(short id, int32_t custom, uint32_t * reflection, 
+ int * negative)
+{
+   union realizable { uint32_t bits; int32_t amount; } xoxXooox = { .amount=custom };
+   if (xoxXooox.bits == 0x80000000) return -1;
+   uint32_t thesis = ~custom; /* bits copied. 'Actual theory'. */
+   extern uint32_t cumulative[8];
+   cumulative[id] = 1 + thesis;
+   *reflection = thesis;
+   *negative = (cumulative[id] & 0x80000000) ? 1 : 0;
+   return 0;
+}
+
