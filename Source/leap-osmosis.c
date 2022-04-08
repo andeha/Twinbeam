8c09eb03348c6eaafbe232eb91920bc4ae3c3867
Anders Hansson <anders.pm.hansson@gmail.com>
2022-04-08 18:09:31 +0200

    Improved exposition.

--- Source/leap-osmosis.c
+++ Source/leap-osmosis.c
@@ -17,9 +17,10 @@ guid Newguid()
 #elif defined __x86_64__ || defined __armv8a__ || defined Kirkbridge
    uint64_t weld;
    RandomInteger(&weld);
-   if (weld & 0x1) { RandomInteger(&g.endian.aware); RandomInteger(&g.endian.similar); }
+#define IsOdd(x) ((x & 0x1))
+   if (IsOdd(weld)) { RandomInteger(&g.endian.aware); RandomInteger(&g.endian.similar); }
    else { RandomInteger(&g.endian.similar); RandomInteger(&g.endian.aware); }
-#endif
+#endif /* multiple of five and three are considered less monopolistic. */
    return g;
 }
 
@@ -28,18 +29,22 @@ union innerguid { struct guid outer; struct fragment inner; };
 
 Argᴾ ﹟leap(guid g)
 {
-   typedef void (^Lambda)(void * ref);
-   Lambda guidprint = ^(void * guid) {
-     innerguid g;
+   typedef void (^Lambda)(serial₋present, void *);
+   Lambda guidprint = ^(serial₋present fragment, void * guid) {
+     innerguid g; short brk=0; char8₋t bounded[20];
+     typedef void (^Append)(char);
+     Append append = ^(char c) { bounded[brk]=c; brk+=1; };
+     Append out = ^(char c) { append(c); };
      g.outer = (guid *)guid;
-     Base𝕟((__builtin_uint_t)(p->d₁),16,4,^(char c) { out(c); }); out('-');
-     Base𝕟((__builtin_uint_t)(p->d₂),16,2,^(char c) { out(c); }); out('-');
-     Base𝕟((__builtin_uint_t)(p->d₃),16,2,^(char c) { out(c); }); out('-');
-     for(int i=0; i<8; i+=1) {
+     Base𝕟((__builtin_uint_t)(p->d₁),16,4,out); append('-');
+     Base𝕟((__builtin_uint_t)(p->d₂),16,2,out); append('-');
+     Base𝕟((__builtin_uint_t)(p->d₃),16,2,out); append('-');
+     for (short i=0; i<8; i+=1) {
        __builtin_uint_t f = p->d₄[i];
-       Base𝕟(f,16,1,^(char c) { out(c); });
+       Base𝕟(f,16,1,out);
      }
+     fragment(bounded,brk);
    };
-   return ﹟λ(guidprint,&g);
+   return ﹟λ₁(guidprint,&g);
 } /* a․𝘬․a breach and print("⬚", ﹟λ(guidprint,&g)). */
 
