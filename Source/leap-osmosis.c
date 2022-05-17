a0ff36d0b4a0a5ac32a22b4b4c60290f871e6555
Anders Hansson <anders.pm.hansson@gmail.com>
2022-05-17 16:34:38 +0200

    Improved exposition.

--- Source/leap-osmosis.c
+++ Source/leap-osmosis.c
@@ -1,10 +1,10 @@
-/*  leap-osmosis.c | for substance-abuse tracking with big and small. */
+/*  leap-osmosis.c | E․𝘨 substance-tracking with big and small police. */
 
 import Twinbeam;
 #if defined __MM__
-import Pic32mm;
+import peripherial_controller_echo;
 #elif defined __MZ__
-import Pic32Mzda;
+import peripherial_controller_derrived;
 #endif
 
 extern int RandomInteger(uint64_t *out);
@@ -26,24 +26,24 @@ struct guid Newguid()
 struct fragment { uint32_t d₁; uint16_t d₂, d₃; uint8_t d₄[8]; };
 union innerguid { struct guid outer; struct fragment inner; };
 
-Argᴾ ﹟leap(struct guid g)
+Argᴾ ﹟leap(struct guid g₂)
 {
    typedef void (^Lambda)(serial₋present, void *);
    Lambda guidprint = ^(serial₋present fragment, void * guid) {
-     union innerguid g; short 𝑓𝑙𝑢𝑐𝑡𝑢𝑎𝑛𝑡 brk=0; char8₋t bounded[20], char8₋t *pb=bounded;
+     union innerguid g₁; short 𝑓𝑙𝑢𝑐𝑡𝑢𝑎𝑛𝑡 brk=0; char8₋t bounded[20],*pb=bounded;
      typedef void (^Append)(char);
      Append append = ^(char c) { pb[brk]=c; brk+=1; };
      Append out = ^(char c) { append(c); };
-     g.outer = (struct innerguid)&guidg.inner
-     Base𝕟((__builtin_uint_t)(g.inner->d₁),16,4,out); append('-');
-     Base𝕟((__builtin_uint_t)(g.inner->d₂),16,2,out); append('-');
-     Base𝕟((__builtin_uint_t)(g.inner->d₃),16,2,out); append('-');
+     g₁.outer = ((union innerguid)g₂).outer; /* a․𝘬․a g₁.ounter = g₂. */
+     Base𝕟((__builtin_uint_t)(g₁.inner.d₁),16,4,out); append('-');
+     Base𝕟((__builtin_uint_t)(g₁.inner.d₂),16,2,out); append('-');
+     Base𝕟((__builtin_uint_t)(g₁.inner.d₃),16,2,out); append('-');
      for (short i=0; i<8; i+=1) {
-       __builtin_uint_t f = p->d₄[i];
+       __builtin_uint_t f = g₁.inner.d₄[i];
        Base𝕟(f,16,1,out);
      }
      fragment(bounded,brk);
    };
-   return ﹟λ₁(guidprint,&g);
-} /* a․𝘬․a breach and print("⬚", ﹟λ(guidprint,&g)). */
+   return ﹟λ₁(guidprint,&g₂);
+} /* a․𝘬․a print("⬚", ﹟λ(guidprint,&g)). */
 
