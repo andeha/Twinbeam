8bbe534ad7c37cc2f1de9cc4b17797e9b03e7ec8
Anders Hansson <anders.pm.hansson@gmail.com>
2022-06-09 14:42:43 +0200

    Improved exposition.

--- Source/pic32-institut.c
+++ Source/pic32-institut.c
@@ -1,6 +1,6 @@
-/*  pic32mz.cpp | sometimes many legs, sometimes organized. */
+/*  pic32-institut.cpp | sometimes many legs, sometimes organized. */
 
-import Twinbase;
+import Twinbeam; /* -D__Pic32MMCuriosity__ and -D__MZDAStarterBoard__ */
 
 /* µA("mips", "r2", x₃, x₄) */
 FOCAL
@@ -15,26 +15,78 @@ Copy8Memory(
    const char * sp = (const char *)src;
    while (bytes--) *dp++ = *sp++;
    return dst;
-} /* ⬷ c𝘧․ copying using a Dma-facility and 'asyncronicity' a․𝘬․a OptimisticAsync8Copy. */
+} /* ⬷ c𝘧․ copying using a asyncronous dma a․𝘬․a OptimisticAsync8Copy. */
 
-inexorable __builtin_int_t 𝟺kbframe₋count(__builtin_int_t bytes) 
+__builtin_int_t backpack₋object₋size(void * ref) 
+{
+  __builtin_uint_t * wordref = (__builtin_uint_t *)ref;
+  return *(wordref - 1);
+}
+
+inexorable __builtin_int_t fourkbframe₋bytecount(__builtin_int_t bytes) 
 { return 1 + bytes/4096; }
 
 void backpack₋free(void * ref)
 {
-  struct sh₋unk { __builtin_int_t bytes; } * start = (sh₋unk *)ref;
-  __builtin_int_t ﹟ = 𝟺kbframe₋count(start->bytes);
-  unsigned expeditionary=1; void * 𝟺kbpages[﹟];
-  for (__builtin_int_t i=0, offset=0; i<﹟; ++i, offset += 4096) { 𝟺kbpages[i] = offset + (uint8_t *)ref; }
-  if (🄕allo⒲(expeditionary,𝟺kbpages,﹟)) { return; }
+  __builtin_int_t bytes = backpack₋object₋size(ref);
+  __builtin_int_t ﹟ = fourkbframe₋bytecount(bytes);
+  unsigned expeditionary=1; void * fourkb[﹟];
+  for (__builtin_int_t i=0, offset=0; i<count; ++i, offset += 4096) {
+    fourkb[i] = offset + (uint8_t *)ref; }
+  if (Fallow(expeditionary,fourkb,﹟)) { return; }
   return;
 }
 
 void * backpack₋alloc(__builtin_int_t bytes)
-{
-  __builtin_int_t ﹟ = 𝟺kbframe₋count(bytes + 1*Wordbytes);
-  unsigned expeditionary=1; void * 𝟺kbframes[﹟];
-  if (ContiguousAcquire(expeditionary,𝟺kbframes,﹟)) { return NULL; }
-  return 𝟺kbframes[0];
-} /* ⬷ a․𝘬․a 'first fit' and consecutive. */
+{ __builtin_int_t Wordbytes=sizeof(__builtin_uint_t);
+  __builtin_int_t ﹟ = fourkbframe₋bytecount(bytes + 1*Wordbytes);
+  __builtin_int_t wordcount = ﹟ / Wordbytes;
+  unsigned expeditionary=1; __builtin_uint_t * fourkbframes[wordcount];
+  if (ContiguousAcquire(expeditionary,fourkbframes,﹟)) { return ΨΛΩ; }
+  return 1 + fourkbframes;
+} /* ⬷ a․𝘬․a 'best fit' and consecutive, not 'first fit'. */
+
+void * Heap₋alloc(__builtin_int_t bytes) { return backpack₋alloc(bytes); }
+__builtin_int_t Heap₋object₋size(void * ref) { return backpack₋object₋size(ref); }
+void Heap₋unalloc(void * ref) { backpack₋free(ref); }
+
+void Fallow(void * ref) { Heap₋unalloc(ref); }
+void * Alloc(__builtin_int_t bytes) { return Heap₋alloc(bytes); }
+
+/*                                               the callee must preserve    
+   zero $0   |  always the value 'zero'.                              No     
+   at   $1   |  assembler temporary.                                  No     
+   v0   $2   |  the returned value for 32-bit and smaller….           No     
+   v1   $3   |  …returned 'hi' value when 64-bits are required.       No     
+   a0   $4   |  first integer argument.                               No     
+   a1   $5   |  second integer argument.                              No     
+   a2   $6   |  third integer argument.                               No     
+   a3   $7   |  fourth integer argument.                              No     
+   t0   $8   |  1:st temporary reg. that must not be preserved.       No     
+   t1   $9   |  2:nd temporary reg. that must not be preserved.       No     
+   t2  $10   |  3:th temporary reg. that must not be preserved.       No     
+   t3  $11   |  4:th temporary reg. that must not be preserved.       No     
+   t4  $12   |  5:th temporary reg. that must not be preserved.       No     
+   t5  $13   |  6:th temporary reg. that must not be preserved.       No     
+   t6  $14   |  7:th temporary reg. that must not be preserved.       No     
+   t7  $15   |  8:th temporary reg. that must not be preserved.       No     
+   s0  $16   |                                                       Yes     
+   s1  $17   |  ''                                                   Yes     
+   s2  $18   |   function prologue copies the register value …       Yes     
+   s3  $19   |   to the stack frame and the function …               Yes     
+   s4  $20   |   epilogue copies the values back to the …            Yes     
+   s5  $21   |   register.                                           Yes     
+   s6  $22   |                                               ''      Yes     
+   s7  $23   |                                                       Yes     
+   t8  $24   |  nineth temporary register.                            No     
+   t9  $25   |  tenth temporary register.                             No     
+   k0  $26   |  kernel and/or _ register.                             No     
+   k1  $27   |  kernel and/or _ register.                             No     
+   gp  $28   |  global pointer, the addr. to static globals.         Yes     
+   sp  $29   |  the last pushed argument on the stack.               Yes     
+⬷ the sp register must be 8-bytes aligned.                                  
+   fp  $30   |  the frame pointer.                                   Yes     
+   ra  $31   |  the address to return to when running a ftn.         Yes   */
+/* Parameters smaller than 32-bits are promoted to 32-bits. */
+/* Sixty-four bits parameters are for little-endian passed in e.g a1:a0 and a3:a2. */
 
--- Source/portable-tree.c
+++ Source/portable-tree.c
@@ -1,52 +1,52 @@
-/*  portable-tree.c | search self and two. */
+/*  portable-tree.c | on small and big that search self and two. */
 
 import Twinbeam;
 
-inexorable int Equal(const union Treeint * l, const union Treeint * r)
-{ return l->keyvalue.key == r->keyvalue.key; } /* also (l.bits ^ r.bits) & ~signbit == 0. */
-inexorable int LessThan(const union Treeint * l, const union Treeint * r)
-{ return l->keyvalue.key < r->keyvalue.key; }
-inexorable int LessthanOrEqual(const union Treeint * l, const union Treeint * r)
-{ return l->keyvalue.key <= r->keyvalue.key; }
+inexorable int Equal(union Treeint l, union Treeint r)
+{ return l.keyvalue.key == r.keyvalue.key; } /* also (l.bits ^ r.bits) & ~signbit == 0. */
+inexorable int LessThan(union Treeint l, union Treeint r)
+{ return l.keyvalue.key < r.keyvalue.key; }
+inexorable int LessthanOrEqual(union Treeint l, union Treeint r)
+{ return l.keyvalue.key <= r.keyvalue.key; }
 
-struct node { Treeint data; struct node *right, *left; };
+struct node { union Treeint cargo; struct node *right, *left; };
 
 inexorable struct node* lookup(struct node* node, union Treeint target) {
    if (node == ΨΛΩ) { return ΨΛΩ; }
-   else { if (Equal(target, node->data)) return(node);
+   else { if (Equal(target,node->cargo)) return(node);
       else {
-        if (LessThan(target, node->data)) return(lookup(node->left, target));
+        if (LessThan(target,node->cargo)) return(lookup(node->left,target));
         else return(lookup(node->right, target));
       }
    }
 }
 
-inexorable struct node* newNode(union Treeint data, ALLOC alloc) {
-   struct node* nodeloc = (struct node*)alloc(sizeof(node));
-   if (nodeloc) { return ΨΛΩ; }
-   struct node* node = new (nodeloc) struct node;
-   node->data = data;
+inexorable struct node* newNode(union Treeint material, ALLOC alloc) {
+   struct node* node = (struct node*)alloc(sizeof(struct node));
+   if (node) { return ΨΛΩ; }
+   /* struct node* nodeloc = new (nodeloc) struct node; */
+   node->cargo = material;
    node->left = ΨΛΩ;
    node->right = ΨΛΩ;
    return(node);
 }
 
 inexorable
-struct node* insert(struct node* node, union Treeint data, ALLOC alloc) {
+struct node* insert(struct node* node, union Treeint material, ALLOC alloc) {
    // 1. If the tree is empty, return a new, single node
-   if (node == ΨΛΩ) { return(newNode(data, alloc)); }
+   if (node == ΨΛΩ) { return(newNode(material, alloc)); }
    else {
      // 2. Otherwise, recur down the tree
-     if (LessthanOrEqual(data, node->data)) { 
-       node->left = insert(node->left, data, alloc); }
-     else { node->right = insert(node->right, data, alloc); }
+     if (LessthanOrEqual(material, node->cargo)) { 
+       node->left = insert(node->left, material, alloc); }
+     else { node->right = insert(node->right, material, alloc); }
      return(node); // Return the (unchanged) node pointer.
    }
 }
 
 union Treeint * Lookup(void ᶿ﹡ opaque, union Treeint leafkey) {
    struct node* tell = lookup((struct node*)opaque, leafkey);
-   if (tell) { return &(tell->data); }
+   if (tell) { return &(tell->cargo); }
    return ΨΛΩ; /* ⬷ `target` not in tree. */
 }
 
@@ -56,7 +56,7 @@ void * Insert(void * opaque, union Treeint valkey, ALLOC alloc)
 void Forall(void ᶿ﹡ opaque, void (^dfs)(union Treeint valkey, int * stop))
 {  struct node* n=(struct node*)opaque; int stop=false;
     if (n == ΨΛΩ) return;
-    dfs(n->data, stop); /* Neither infix nor postfix! Prefix. */
+    dfs(n->cargo, &stop); /* Neither infix nor postfix! Prefix. */
     if (stop) return;
     Forall(n->left, dfs);
     Forall(n->right, dfs);
