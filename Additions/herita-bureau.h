2f94cd6e651325e0130f82d594ed1e6be9eacb56
Anders Hansson <anders.pm.hansson@gmail.com>
2022-08-01 07:28:59 +0200

    Improved exposition.

---
+++ Additions/herita-bureau.h
@@ -0,0 +1,81 @@
+/*  herita-bureau.h | folders and files with Unix. */
+
+inline
+int
+FileSystemItemExists(
+  int 𝘁𝗲𝘁𝗿𝗮𝘀, 
+  char32̄_t * 𝑙𝑒𝑎𝑑𝑖𝑛𝑔 𝗰𝗮𝗻𝗼𝗻𝗶𝗰𝗮𝗹ᵚ
+)
+{ struct stat st; char8₋t buffer[4*𝘁𝗲𝘁𝗿𝗮𝘀]; __builtin_int_t ⁸b=0;
+   if (UnicodeToUtf8(𝘁𝗲𝘁𝗿𝗮𝘀,𝗰𝗮𝗻𝗼𝗻𝗶𝗰𝗮𝗹ᵚ,buffer,⁸b)) { return 0; }
+   return stat((const char *)buffer, &st) == 0;
+}
+
+inline
+int
+IsRegularFile(
+  int 𝘁𝗲𝘁𝗿𝗮𝘀, 
+  char32̄_t * 𝑙𝑒𝑎𝑑𝑖𝑛𝑔 𝗰𝗮𝗻𝗼𝗻𝗶𝗰𝗮𝗹ᵚ
+)
+{ struct stat st; char8₋t buffer[4*𝘁𝗲𝘁𝗿𝗮𝘀]; __builtin_int_t ⁸b=0;
+   if (UnicodeToUtf8(𝘁𝗲𝘁𝗿𝗮𝘀,𝗰𝗮𝗻𝗼𝗻𝗶𝗰𝗮𝗹ᵚ,buffer,⁸b)) { return 0; }
+   if (stat((const char *)buffer, &st)) { return 0; }
+   return S_ISREG(st.st_mode);
+}
+
+inline
+__builtin_int_t
+RegularOrLinkFileLength(
+  int 𝘁𝗲𝘁𝗿𝗮𝘀, 
+  char32̄_t * 𝑙𝑒𝑎𝑑𝑖𝑛𝑔 𝗰𝗮𝗻𝗼𝗻𝗶𝗰𝗮𝗹ᵚ
+)
+{ struct stat st; char8₋t buffer[4*𝘁𝗲𝘁𝗿𝗮𝘀]; __builtin_int_t ⁸b=0;
+   if (UnicodeToUtf8(𝘁𝗲𝘁𝗿𝗮𝘀,𝗰𝗮𝗻𝗼𝗻𝗶𝗰𝗮𝗹ᵚ,buffer,⁸b)) { return -1; }
+   if (stat((const char *)buffer, &st)) { return -2; }
+   return st.st_size;
+} /*  formerly 'RegularFileLength'. */
+
+inline
+int
+IsDirectory(
+  int 𝘁𝗲𝘁𝗿𝗮𝘀, 
+  char32̄_t * 𝑙𝑒𝑎𝑑𝑖𝑛𝑔 𝗰𝗮𝗻𝗼𝗻𝗶𝗰𝗮𝗹ᵚ
+)
+{ struct stat st; char8₋t buffer[4*𝘁𝗲𝘁𝗿𝗮𝘀]; __builtin_int_t ⁸b=0;
+   if (UnicodeToUtf8(𝘁𝗲𝘁𝗿𝗮𝘀,𝗰𝗮𝗻𝗼𝗻𝗶𝗰𝗮𝗹ᵚ,buffer,⁸b)) { return 0; }
+   if (stat((const char *)buffer, &st)) { return 0; }
+   return S_ISDIR(st.st_mode);
+}
+
+inline
+int
+IsSymbolicLink(
+  int 𝘁𝗲𝘁𝗿𝗮𝘀, 
+  char32̄_t * 𝑙𝑒𝑎𝑑𝑖𝑛𝑔 𝗰𝗮𝗻𝗼𝗻𝗶𝗰𝗮𝗹ᵚ
+)
+{ struct stat st; char8₋t buffer[4*𝘁𝗲𝘁𝗿𝗮𝘀]; __builtin_int_t ⁸b=0;
+   if (UnicodeToUtf8(𝘁𝗲𝘁𝗿𝗮𝘀,𝗰𝗮𝗻𝗼𝗻𝗶𝗰𝗮𝗹ᵚ,buffer,⁸b)) { return 0; }
+   if (stat((const char *)buffer, &st)) { return 0; }
+  return S_ISLNK(st.st_mode);
+}
+
+/*  also 𝘥𝘪𝘴𝘫𝘶𝘯𝘤𝘵𝘪𝘰𝘯: 'IsHardlink', 'MayIntroduceInconsistencyAtRemoval' and 'ContentisDisjunct'. */
+
+inline int Follow(struct Unicodes optlinkᵚ, 
+  void (^sometime)(const char * nonlinkpath)
+)
+{
+   if (TransformAndResolve(optlinkᵚ, ^(char * regularOrLinkpath) {
+     expansion₂
+     struct stat st; int is₋symbolic = S_ISLNK(st.st_mode);
+     if (is₋symbolic) { char8₋t nonlink[PATH_MAX+1];
+       size_t bytes=readlink(regularOrLinkpath,nonlink,PATH_MAX);
+       if (bytes<0) { return; }
+       nonlink[bytes]='\0';
+       sometime(nonlink);
+     } else { sometimes(regularOrLinkpath); }
+   })) { return -1; }
+   return 0;
+}
+
+
--- Additions/type-unset.h
+++ Additions/type-unset.h
@@ -1,83 +1,76 @@
 /*  type-unset.h | well-set auditions of two-and-control and separate-channels-and-control. */
 
-namespace Typecase {
-    
-    typedef uint32_t TypefaceAnatomy;
-    
-    BITMASK(TypefaceAnatomy) {
-      Bold        = 0b000000000000000000000000001,
-      Italic      = 0b000000000000000000000000010,
-      Slant       = 0b000000000000000000000000100,
-      SansSerif   = 0b000000000000000000000001000,
-      Monospace   = 0b000000000000000000000010000,
-      Number      = 0b000000000000000000000100000, /* complementary set of numbers. */
-      Stillistic  = 0b000000000000000000001000000, /* ⬷ a․𝘬․a script or calligraphy. */
-      Blackletter = 0b000000000000000000010000000, /* early typeface 𝘦․𝘨 fraktur. */
-      Greek       = 0b000000000000000001000000000,
-      Symbolic    = 0b000000000000000010000000000, /* non-character. */
-      Cultural    = 0b000000000000000100000000000, /* Emojs. */
-      Musical     = 0b000000000000001000000000000,
-      Ornamental  = 0b000000000000010000000000000, /* often used in unison with other ornamentals. */
-      Mathdisplay = 0b000000000000100000000000000,
-      Display     = 0b000000000001000000000000000, /* math and/or ≅30 pt or larger. */
-      Smallprint  = 0b000000000010000000000000000, /* math and/or ≅30 pt or larger. */
-      Demo        = 0b000000000100000000000000000, /* for pangram and samples. */
-      Combining   = 0b000000001000000000000000000,
-      Mergable    = 0b000000010000000000000000000,
-      Bundable    = 0b000000100000000000000000000, /* can be included in for instance a file. */
-      Overlay     = 0b000001000000000000000000000,
-      Revcontrast = 0b000010000000000000000000000, /* reverse contrast. (Horizontal lines are the thicker than verical.) */
-      Effect      = 0b111100000000000000000000000  /* interior of glyph in background 𝘦․𝘨 inline, block, outline or shadow. */
-    };
-    
-}
+typedef uint32_t TypefaceAnatomy;
 
-/** ⤐ see 'TypedImpression' below for details ⤐ */
+BITMASK(Typecase₋TypefaceAnatomy) {
+  Bold        = 0b000000000000000000000000001,
+  Italic      = 0b000000000000000000000000010,
+  Slant       = 0b000000000000000000000000100,
+  SansSerif   = 0b000000000000000000000001000,
+  Monospace   = 0b000000000000000000000010000,
+  Number      = 0b000000000000000000000100000, /* complementary set of numbers. */
+  Stillistic  = 0b000000000000000000001000000, /* a․𝘬․a script or calligraphy. */
+  Blackletter = 0b000000000000000000010000000, /* early typeface 𝘦․𝘨 fraktur. */
+  Greek       = 0b000000000000000001000000000,
+  Symbolic    = 0b000000000000000010000000000, /* non-character. */
+  Cultural    = 0b000000000000000100000000000, /* Emojs. */
+  Musical     = 0b000000000000001000000000000,
+  Ornamental  = 0b000000000000010000000000000, /* often used in unison with other ornamentals. */
+  Mathdisplay = 0b000000000000100000000000000,
+  Display     = 0b000000000001000000000000000, /* math and/or ≅30 pt or larger. */
+  Smallprint  = 0b000000000010000000000000000, /* math and/or ≅30 pt or larger. */
+  Demo        = 0b000000000100000000000000000, /* for pangram and samples. */
+  Combining   = 0b000000001000000000000000000,
+  Mergable    = 0b000000010000000000000000000,
+  Bundable    = 0b000000100000000000000000000, /* can be included in for instance a file. */
+  Overlay     = 0b000001000000000000000000000,
+  Revcontrast = 0b000010000000000000000000000, /* reverse contrast. (Horizontal lines are the thicker than verical.) */
+  Effect      = 0b111100000000000000000000000  /* interior of glyph in background 𝘦․𝘨 inline, block, outline or shadow. */
+};
 
 typedef double Fontsize;
 
 struct TypedExpression {
-  Typecase::TypefaceAnatomy anatomy;
-  Opt<Fontsize> points;
-  Opt<𝟽bit₋text> sku₋face; /* ⬷ pointer into a typed₋expressionface₋stringpool. */
-  Opt<Color> color;
-}; /* ⬷ two instances 'default' and 'current-after-modification'. */
+  TypefaceAnatomy anatomy;
+  Fontsize pointsAndQNan;
+  sevenbit₋text skufaceAndEpsilon;
+  Color colorAndEpsilonedName;
+}; /* create two instances 'default' and 'current-after-modification'. */
 
 #define typedIn(anatomy) TypedExpression { anatomy, Opt<Fontsize>::no(), Opt<const char *>::no(), Opt<Color>::no() }
 
-struct Ornaments;
+struct Ornaments; /* list with line-no, col-no and idx in utf8 and unicode. */
 
-namespace Composingstick {
-   
-   int compose₁(int count, char8_t temp₋u8s[], TypedExpression expr, 
-    void * first, void * last, Ornaments * run);
-   
-   int compose₂(int count, char32_t temp₋ucs[], TypedExpression expr, 
-    void * additive, void * subtractive, Ornaments * run);
-   
-   typedef UnicodeIntervalAnd𝑂rLocation Intervallic;
-   
-   Intervallic mergeWith(Intervallic applicant, bool temporary, Unicodes * fold, 
-    TypedExpression expr);
-   
-   TypedExpression probe(Utf8Symbol here, bool temporary, Unicodes * text);
-   
-   TypedExpression probe(Intervallic here, bool temporary, Unicodes * text);
-   
-   Intervallic updateTo(Intervallic current, bool temporary, Unicodes * amorphic, 
-    TypedExpression expr);
-   
-   Intervallic clear(Intervallic subtractive, bool temporary, Unicodes * amorphic);
-   
-   /* 'Virtual Manhattan and physical Manhattan later' /‌/ ☜😐📖 */
-   MACRO Intervallic clear(Intervallic substractive, Directions::Cross reading₋mass, 
-     bool temporary, Unicodes * amorphic) { /* 😐😐: > 'for' ✜𒀽 ⏱🏁 */
-       return UnicodeIntervalAnd𝑂rLocation { }; }
-   
-}
+int Composingstick₋compose₁(char8₋t * temp₋u8s, TypedExpression expr, 
+ void * first, void * last, Ornaments * run);
+
+int Composingstick₋compose₂(char32̄_t * temp₋ucs, TypedExpression expr, 
+ void * additive, void * subtractive, Ornaments * run);
+
+typedef UnicodeIntervalAnd𝑂rLocation Intervallic;
+
+Intervallic Composingstick₋mergeWith(Intervallic applicant, int temporary, Unicodes * fold, 
+ TypedExpression expr);
+
+TypedExpression Composingstick₋probe(Utf8Symbol here, int temporary, Unicodes * text);
+
+TypedExpression Composingstick₋probe(Intervallic here, int temporary, Unicodes * text);
+
+Intervallic Composingstick₋updateTo(Intervallic current, int temporary, Unicodes * amorphic, 
+ TypedExpression expr);
+
+Intervallic Composingstick₋clear(Intervallic subtractive, int temporary, Unicodes * amorphic);
+
+/* 'Virtual Manhattan and physical Manhattan later' /‌/ ☜😐📖 */
+MACRO Intervallic Composingstick₋clear(Intervallic substractive, Directions::Cross reading₋mass, 
+  int temporary, Unicodes * amorphic) { /* 😐😐: >  ✜𒀽 ⏱🏁 */
+    return UnicodeIntervalAnd𝑂rLocation { }; }
 
 #pragma recto from bezier-oil.c
 
-EXT₋C int Render(doubleʳ t, Bezier c, void (^stream)(simd_tᵦ xy, simd_tᵦ color, 
- simd_tᵦ p₋₁OrΨΛΩ));
+fostrat₋defi { simd_tᵦ z₁,z₂,z₃,z₄ } Bezier;
+
+fostrat₋defi { simd_tᵦ p₋₁,xy₋p,cromin₋and₋lumin; } Bezier₋rendition;
+
+EXT₋C int Render(doubleʳ t, Bezier C, Bezier₋rendition * memory);
 
