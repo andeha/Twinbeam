d9686ae9c0f1706217c3517dca422f5c504ef2c6
Anders Hansson <anders.pm.hansson@gmail.com>
2022-08-01 12:39:43 +0200

    Improved exposition.

---
+++ twotree-random.c
@@ -0,0 +1,14 @@
+/*  twotree-random.c | chlorophylle visits torpille. */
+
+import Twinbeam;
+
+int either(const char * order, int * two₋alternatively₋three)
+{
+   /* ... */
+} /* fõrst ār det lika. sedan inte 'det' utan betan. */
+
+int main()
+{
+   return 0;
+}
+
