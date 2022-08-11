2db2e106130c9955e6399e24e0a9f62333b79a2c
Anders Hansson <anders.pm.hansson@gmail.com>
2022-08-11 19:38:12 +0200

    Improved exposition.

--- Additions/amorph-rax.h
+++ Additions/amorph-rax.h
@@ -5,23 +5,50 @@ union graph₋utility {
   struct graph₋arc * A;
   struct subgraph * G;
   unicode₋shatter S;
+  keyput₋shatter s;
   __builtin_int_t I;
 };
+
 struct graph₋vertex {
-  struct graph₋arc * arcs;
   unicode₋shatter name;
   union graph₋utility u,v,w,y,z;
 };
+
 struct graph₋arc {
-  struct graph₋vertex * tip;
-  struct graph₋arc * next;
   __builtin_int_t length;
   union graph₋utility a,b;
 };
-typedef struct subgraph {
-  struct graph₋vertex * vertices;
+
+struct vertex₋block {
+  struct graph₋vertex * start;
+  __builtin_int_t count;
+};
+
+struct edge₋block {
+  struct graph₋edge * start;
+  __builtin_int_t count;
+};
+
+struct incidence₋element {
+  struct vertex₋block * src₋set, *dst₋set;
+  struct edge₋block * edge₋set;
+  __builtin_int_t edge₋idx,dst₋idx,src₋idx;
+};
+
+struct incidence₋partial {
+  struct incidence₋element * set;
+  __builtin_int_t edge₋idx;
+}; /*  a․𝘬․a partial₋incidence₋function. */
+
+fostrat₋defi subgraph {
+  struct vertex₋block * tip;
+  struct edge₋block * tip;
+  struct incidence₋partial * tip;
   __builtin_int_t n,m;
-  char util₋types[15];
+  char util₋types[15]; /* P=subgraph₋next,N=subgraph₋prev,C=connection, 
+   L=Unicode₋label,l=keyput₋label,I=weight. */
   union graph₋utility uu,vv,ww,xx,yy,zz;
 } Graph;
+
+EXT₋C int strong₋and₋weak(Graph * g, Graph ** SAndΨΛΩ, Graph ** WAndΨΛΩ);
 
