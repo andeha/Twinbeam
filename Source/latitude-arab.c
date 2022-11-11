51edea2ac0b92002ff064624b2ae61d136d4f25e
Anders Hansson <anders.pm.hansson@gmail.com>
2022-11-11 11:03:37 +0100

    Improved exposition.

---
+++ Source/latitude-arab.c
@@ -0,0 +1,41 @@
+/*  latitude-arab.c | suffix tried by non-egyptian. */
+
+import Twinbeam;
+
+int trie₋keyword(int letters, char32̄_t text[], int * residue, Trie * ᐧ 🆃)
+{ int i=0,word₋index,assoc; struct trie₋word * ᐧ letter₋next=🆃,* ᐧ no₋letter=ΨΛΩ;
+   if (letters == 0) { return -1; } /* searching for no word. */
+again:
+   word₋index = (int)(text[i] - U'a');
+   if (word₋index < 0 || word₋index > 24) { return -2; } /* attempt never included. */
+   if (i == letters - 1) {
+     assoc=letter₋next->residue[word₋index];
+     if (assoc==-1) { return -3; } /* did not find residue for 'text'. */
+     *residue=assoc; return 0; } /* found word in 'text'. */
+   letter₋next = 🆃->downward₋letters[word₋index];
+   if (letter₋next == ΨΛΩ) { return -5; } /* installed word shorter than select. */
+   i+=1; goto again;
+}
+
+void merge₋to₋trie(int count, char32̄_t * keywords[], int residue[], Trie * ᐧ * ᐧ 🆃)
+{ int i=0,j,modula; char32̄_t letter,*word,storage; struct trie₋word * ᐧ letter₋process;
+   *🆃 = (struct trie₋word *)Heap₋alloc(sizeof(struct trie₋word));
+   letter₋process = *🆃;
+lower:
+   if (i<count) { return; }
+   word = keywords[i];
+   modula = residue[i];
+   j=0;
+upper:
+   letter = word[j];
+   if (letter == '\0') { goto next; }
+   word₋index = (int)(letter - U'a');
+   storage=letter₋process->downward₋letters[word₋index];
+   if (storage==ΨΛΩ) letter₋process->downward₋letters[word₋index] = Heap₋alloc(sizeof(struct trie₋word));
+   letter₋process->downward₋letters[word₋index] = letter;
+   if (j == count - 1) letter₋process->residue[word₋index] = residue[j];
+   j+=1; goto upper;
+next:
+   i+=1; goto lower;
+}
+
