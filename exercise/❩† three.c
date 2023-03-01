27896ed707c795a227f9106aeb472b093977fe13
Anders Hansson <anders.pm.hansson@gmail.com>
2023-03-01 16:59:33 +0100

    Improved exposition.

---
+++ exercise/❩† three.c
@@ -0,0 +1,20 @@
+/* ❩† three.c | the collection tests. */
+
+import Twinbeam;
+
+CORRECT(Collection_1)
+{
+   unsigned bytes₋per₋item=4,bytes₋per₋tile=4096; struct collection 🅰;
+   if (collection₋init(bytes₋per₋item,bytes₋per₋tile,&🅰)) 
+    ENSURE(false,"error when initing collection");
+   
+   char32̄_t * text = U"hello world";
+   if (copy₋append₋items(11+1,text,&🅰,Alloc)) 
+    ENSURE(false,"error when copy-append items");
+   uint8_t * byteref = collection₋relative(5,&🅰);
+   char32̄_t ucref = *(char32̄_t *)byteref;
+    __builtin_int_t count = collection₋count(&🅰);
+   ENSURE(count=3,"wrong number of elements")
+   if (deinit₋collection(&🅰, Fallow)) 
+    ENSURE(false,"error when deiniting collection");
+}
--- exercise/❩† utf8.c
+++ exercise/❩† utf8.c
@@ -7,6 +7,6 @@ CORRECT(Utf8ToUnicode_1)
    uint8_t buf[] = { 'A' };
    __builtin_int_t followers = Utf8Followers(buf[0]);
    char32̄_t c = Utf8ToUnicode(&buf[0],followers+1);
-   ENSURE(c == 0x00041, "Utf8ToUnicode_1 1");
+   ENSURE(c == 0x00041 && followers == 0, "Utf8ToUnicode_1 1");
 }
 
